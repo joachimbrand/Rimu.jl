7c1f9fbfcf837d3000d20abcd39e4f4aee2578a6
Joachim Brand <j.brand@massey.ac.nz>
2019-08-13 17:34:42 +1200

    fixed docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,13 @@
+push!(LOAD_PATH,"src/")
 using Documenter, Rimu
 
 makedocs(;
     modules=[Rimu],
-    format=Documenter.HTML(),
+    format=Documenter.HTML(prettyurls = false),
     pages=[
         "Home" => "index.md",
     ],
     repo="https://bitbucket.org/joachimbrand/Rimu.jl/blob/{commit}{path}#L{line}",
     sitename="Rimu.jl",
     authors="Joachim Brand <j.brand@massey.ac.nz>",
-    assets=String[],
 )
