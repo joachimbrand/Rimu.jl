374db9f3fc1ce642f9572af5e3f922f4ec9dbad8
Joachim Brand <j.brand@massey.ac.nz>
2019-09-24 17:17:28 +1200

    write to file from read_file_and_plot.jl

--- scripts/read_file_and_plot.jl
+++ scripts/read_file_and_plot.jl
@@ -1,6 +1,14 @@
+writetofile = false
+showplots = true
+
 using Feather, DataFrames, Rimu
-using PyPlot
-pygui(true)
+showplots && using PyPlot
+showplots && pygui(true)
+
+filename = "ba.out"
+io = stdout
+writetofile && (io = open(filename,"w"))
+
 
 # look for "plotting.jl" in the same folder as this script
 scriptsdir = @__DIR__
@@ -8,20 +16,22 @@ include(joinpath(scriptsdir,"plotting.jl"))
 
 # read data
 df = Feather.read("fciqmcdata.feather")
-println("Number of time steps: ", size(df,1)-1)
+println(io, "Number of time steps: ", size(df,1)-1)
 
 # plot stats and energy
-plotQMCStats(df)
-plotQMCEnergy(df)
+showplots && plotQMCStats(df)
+showplots && plotQMCEnergy(df)
 
-# analyse energy data 
-start_blocking = 2_000 # put a meaningful value in
-println("Starting blocking analysis at time step ", start_blocking)
+# analyse energy data
+start_blocking = 50_000 # put a meaningful value in
+println(io, "Starting blocking analysis at time step ", start_blocking)
 ba_shift  = blocking(df[start_blocking:end,:shift])
 qmcEnergy = ba_shift[1,:mean]
 qmcEnergyError = ba_shift[mtest(ba_shift),:std_err]
 block_steps = df[start_blocking:end,:steps]
-fill_between(block_steps,qmcEnergy-qmcEnergyError,
+showplots && fill_between(block_steps,qmcEnergy-qmcEnergyError,
     qmcEnergy+qmcEnergyError,facecolor="m",alpha=0.3)
-plot(block_steps,ones(length(block_steps))*qmcEnergy,"--m")
-println("Final result for ground state eigenvalue (from shift): $qmcEnergy ± $qmcEnergyError")
+showplots && plot(block_steps,ones(length(block_steps))*qmcEnergy,"--m")
+println(io, "Final result for ground state eigenvalue (from shift): $qmcEnergy ± $qmcEnergyError")
+
+writetofile && close(io)
