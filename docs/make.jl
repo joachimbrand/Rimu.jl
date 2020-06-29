d7ef7690b6ff9e06ad19b2b03f60ca3be4e32bc8
Ray <mingrui.yang@outlook.com>
2020-06-29 17:54:58 +1200

    try again with TRAVIS_REPO_SLUG

--- docs/make.jl
+++ docs/make.jl
@@ -3,15 +3,16 @@
 include("build.jl") # generates the html pages
 
 # For deploydocs, you need to set up the following TRAVIS_* environment variables.
-# withenv(
-#     # TRAVIS_REPO_SLUG is the only one that is repository-specific. This is because we are
-#     # actually deploying the HTML pages into a different repository.
-#     "TRAVIS_REPO_SLUG" => "bitbucket.org/joachimbrand/joachimbrand.bitbucket.io",
-#     "TRAVIS_PULL_REQUEST" => get(ENV, "BITBUCKET_BRANCH", nothing),
-#     "TRAVIS_BRANCH" => get(ENV, "BITBUCKET_BRANCH", nothing),
-#     "TRAVIS_TAG" => get(ENV, "BITBUCKET_TAG", nothing),
-#     "TRAVIS_PULL_REQUEST" => ("BITBUCKET_PR_ID" in keys(ENV)) ? "true" : "false",
-# ) do
+withenv(
+    # TRAVIS_REPO_SLUG is the only one that is repository-specific. This is because we are
+    # actually deploying the HTML pages into a different repository.
+    #"TRAVIS_REPO_SLUG" => "bitbucket.org/joachimbrand/joachimbrand.bitbucket.io",
+    "TRAVIS_REPO_SLUG" => "github.com/myang04/rimudoc"
+    "TRAVIS_PULL_REQUEST" => get(ENV, "BITBUCKET_BRANCH", nothing),
+    "TRAVIS_BRANCH" => get(ENV, "BITBUCKET_BRANCH", nothing),
+    "TRAVIS_TAG" => get(ENV, "BITBUCKET_TAG", nothing),
+    "TRAVIS_PULL_REQUEST" => ("BITBUCKET_PR_ID" in keys(ENV)) ? "true" : "false",
+) do
 deploydocs(
     # repo here needs to point to the BitBucket Pages repository
     # repo = "bitbucket.org/joachimbrand/joachimbrand.bitbucket.io.git",
@@ -23,5 +24,5 @@ deploydocs(
     # it is best to deploy the docs to a subdirectory named after the package
     # dirname = "Rimu.jl",
 )
-# end
+end
 # @info "Documentation updated on https://joachimbrand.bitbucket.io/Rimu.jl/dev/ "
