82ebbbcfac30f057acb02440980e783096217144
Joachim Brand <j.brand@massey.ac.nz>
2021-01-13 22:57:24 +1300

    Add module RimuIO for saving DataFrames in Arrow format files.

---
+++ src/RimuIO.jl
@@ -0,0 +1,29 @@
+"""
+Module to provide file inut and output functionality for `Rimu`.
+Provides convenience functions:
+
+* [`RimuIO.save(filename, df::DataFrame)`](@ref) Save dataframe in Arrow format.
+* [`RimuIO.load(filename)`](@ref) Load Arrow file into dataframe.
+"""
+module RimuIO
+
+using Arrow, DataFrames
+
+function __init__()
+    Arrow.ArrowTypes.registertype!(Complex{Int},Complex{Int})
+    Arrow.ArrowTypes.registertype!(Complex{Float64},Complex{Float64})
+end
+
+"""
+    RimuIO.save(filename, df::DataFrame)
+Save dataframe in Arrow format.
+"""
+save(filename, df::DataFrame) = Arrow.write(filename, df)
+
+"""
+    RimuIO.load(filename) -> DataFrame
+Load Arrow file into dataframe.
+"""
+load(filename) = DataFrame(Arrow.Table(filename))
+
+end # module RimuIO
