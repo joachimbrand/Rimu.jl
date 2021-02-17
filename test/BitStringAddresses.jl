b81fe163fcd59966ae7a3bb8725c5f5dc40e155d
mtsch <matijacufar@gmail.com>
2021-02-18 10:18:15 +1300

    implement occupied_orbitals iterator

--- test/BitStringAddresses.jl
+++ test/BitStringAddresses.jl
@@ -1,6 +1,6 @@
 using Rimu
 using Rimu.BitStringAddresses
-using Rimu.BitStringAddresses: check_consistency
+using Rimu.BitStringAddresses: check_consistency, occupied_orbitals
 using StaticArrays
 using Test
 
@@ -80,19 +80,24 @@ end
 using Rimu.Hamiltonians: numberoccupiedsites, bosehubbardinteraction
 
 @testset "BoseFS" begin
-    middle_full = BoseFS{66,100}(BitAdd{166}(SVector(1, ~UInt64(0), UInt64(1) << 63)))
-    middle_empty = BoseFS{8,150}(BitAdd{158}(SVector{3,UInt64}(255, 0, 0)))
+    middle_full = BoseFS{67,100}(BitAdd{166}(
+        SVector(1, ~UInt64(0), UInt64(1) << 63 | UInt64(2))
+    ))
+    middle_empty = BoseFS{10,150}(BitAdd{158}(SVector{3,UInt64}(255, 0, 3)))
     two_full = BoseFS{136,136}(BitAdd{158}(SVector{3,UInt64}(255, ~UInt64(0), ~UInt64(0))))
     @testset "onr" begin
         middle_full_onr = onr(middle_full)
         @test length(middle_full_onr) == 100
-        @test middle_full_onr[64] == 66
-        @test all(iszero, middle_full_onr[[1:63; 65:end]])
+        @test middle_full_onr[63] == 66
+        @test middle_full_onr[2] == 1
+        @test middle_full_onr[1] == 0
+        @test all(iszero, middle_full_onr[[3:62; 64:end]])
 
         middle_empty_onr = onr(middle_empty)
         @test length(middle_empty_onr) == 150
-        @test middle_empty_onr[129] == 8
-        @test all(iszero, middle_empty_onr[[1:128; 130:end]])
+        @test middle_empty_onr[1] == 2
+        @test middle_empty_onr[127] == 8
+        @test all(iszero, middle_empty_onr[[2:126; 128:end]])
 
         two_full_onr = onr(two_full)
         @test length(two_full_onr) == 136
@@ -100,25 +105,46 @@ using Rimu.Hamiltonians: numberoccupiedsites, bosehubbardinteraction
         @test all(iszero, two_full_onr[2:end])
     end
     @testset "numberoccupiedsites" begin
-        @test numberoccupiedsites(middle_full) == 1
-        @test numberoccupiedsites(middle_empty) == 1
+        @test numberoccupiedsites(middle_full) == 2
+        @test numberoccupiedsites(middle_empty) == 2
         @test numberoccupiedsites(two_full) == 1
     end
     @testset "bosehubbardinteraction" begin
         @test bosehubbardinteraction(middle_full) == 66 * 65
-        @test bosehubbardinteraction(middle_empty) == 8 * 7
+        @test bosehubbardinteraction(middle_empty) == 8 * 7 + 2
         @test bosehubbardinteraction(two_full) == 136 * 135
     end
 
-    function rand_onr(N, M)
-        result = zeros(MVector{M,Int})
-        for _ in 1:N
-            result[rand(1:M)] += 1
-        end
-        return result
+    @testset "occupied_orbitals" begin
+        (bosons, orbital, bit), st = iterate(occupied_orbitals(middle_full))
+        @test bosons == 1
+        @test orbital == 2
+        @test bit = 1
+        (bosons, orbital, bit), st = iterate(occupied_orbitals(middle_full), st)
+        @test bosons == 66
+        @test orbital == 63
+        @test bit = 63
+        @test isnothing(iterate(occupied_orbitals(middle_full), st))
     end
 
     @testset "Randomized tests" begin
+        function rand_onr(N, M)
+            result = zeros(MVector{M,Int})
+            for _ in 1:N
+                result[rand(1:M)] += 1
+            end
+            return result
+        end
+        # This function checks if iteration works ok.
+        function onr2(bose::BoseFS{N,M}) where {N,M}
+            result = zeros(MVector{M,Int32})
+            for (n, i, _) in occupied_orbitals(bose)
+                @assert n ≠ 0
+                result[i] = n
+            end
+            return SVector(result)
+        end
+
         for _ in 1:10
             for (N, M) in ((16, 16), (64, 32), (200, 200), (100, 100), (200, 20), (20, 200))
                 input = rand_onr(N, M)
@@ -131,6 +157,8 @@ using Rimu.Hamiltonians: numberoccupiedsites, bosehubbardinteraction
                     @show input
                 end
                 @test bosehubbardinteraction(bose) == sum(input .* (input .- 1))
+
+                @test onr2(bose) == input
             end
         end
     end
