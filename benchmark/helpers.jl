4b99c2db3ebbbaf6a4895b65e570f487489948ae
mtsch <matijacufar@gmail.com>
2021-03-08 11:11:10 +1300

    update benchmarks

---
+++ benchmark/helpers.jl
@@ -0,0 +1,25 @@
+"""
+    run_lomc(H, addr; n_steps, Ntw, dτ)
+
+Function used to run lomc! benchmarks. Does the necessary setup and runs.
+"""
+function run_lomc(H, addr; n_steps, Ntw, dτ)
+    Nw_init = 10
+    k = 1
+
+    c = DVec2(Dict(addr => Nw_init), Ntw)
+    params = RunTillLastStep(; step=0, dτ=dτ, laststep=n_steps)
+    s_strat = DoubleLogUpdate(targetwalkers=Ntw, ζ=0.08)
+    τ_strat = ConstantTimeStep()
+    r_strat = EveryTimeStep(; projector=copytight(c))
+    Rimu.ConsistentRNG.seedCRNG!(17)
+
+    return lomc!(
+        H, c;
+        params=params,
+        laststep=n_steps,
+        s_strat=s_strat,
+        r_strat=r_strat,
+        τ_strat=τ_strat
+    )
+end
