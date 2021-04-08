229a8a0a80f2fdf8aefdd03206402c8eef0be3ff
mtsch <matijacufar@gmail.com>
2021-04-08 15:30:07 +1200

    try slightly different exchange

--- src/RMPI/async.jl
+++ src/RMPI/async.jl
@@ -24,6 +24,11 @@ function MPIAsynchronous(::Type{Pair{K,V}}, np, id, comm) where {K,V}
     datatype = MPI.Datatype(P)
     return MPIAsynchronous{P,np,typeof(datatype)}(np, id, comm, datatype, ntuple(_ -> P[], np))
 end
+function other_ranks(s::MPIAsynchronous{<:Any,N}) where {N}
+    ntuple(Val(N - 1)) do i
+        mod(s.id + i, s.np)
+    end
+end
 
 function sendbuff(s::MPIAsynchronous, id)
     return s.buffers[id + 1]
@@ -33,24 +38,21 @@ function recvbuff(s::MPIAsynchronous)
 end
 
 function send!(s::MPIAsynchronous{<:Any,N}) where {N}
-    for id in 0:N-1
-        id == s.id && continue
-        sb = sendbuff(s, id)
-        MPI.Isend(MPI.Buffer(sb, length(sb), s.datatype), id, 0, s.comm)
+    for id in other_ranks(s)
+        id == s.id && error()
+        buffer = sendbuff(s, id)
+        MPI.Isend(MPI.Buffer(buffer, length(buffer), s.datatype), id, 0, s.comm)
     end
 end
 
 function receive!(target, s::MPIAsynchronous{<:Any,N}) where {N}
-    for id in 0:N-1
-        id == s.id && continue
-        ready, status = MPI.Iprobe(id, 0, s.comm)
-        #while !ready
-        #    ready, status = MPI.Iprobe(id, 0, s.comm)
-        #end
+    for id in reverse(other_ranks(s))
+        id == s.id && error()
+        status = MPI.Probe(id, 0, s.comm)
         count = MPI.Get_count(status, s.datatype)
         buffer = recvbuff(s)
         resize!(buffer, count)
-        MPI.Irecv!(MPI.Buffer(buffer, length(buffer), s.datatype), id, 0, s.comm)
+        MPI.Recv!(MPI.Buffer(buffer, length(buffer), s.datatype), id, 0, s.comm)
         for (key, val) in buffer
             target[key] += val
         end
@@ -72,7 +74,6 @@ function Rimu.sort_into_targets!(
         end
     end
     send!(s)
-    MPI.Barrier(s.comm)
     receive!(target, s)
 
     return target
