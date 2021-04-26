dcb7bd219a509afac84a8213ed91a213e1fd3e62
Joachim Brand <j.brand@massey.ac.nz>
2021-04-27 00:27:17 +1200

    add @inbounds

--- src/Blocking.jl
+++ src/Blocking.jl
@@ -57,7 +57,7 @@ function blocker(v::AbstractVector{T}) where T
     P = typeof(zero(T)/1)
     new_v = Array{P}(undef,(length(v)÷2))
     for i  in 1:length(v)÷2
-        new_v[i] = (v[2i-1]+v[2i])/2
+        @inbounds new_v[i] = (v[2i-1]+v[2i])/2
     end
     return new_v
 end
@@ -65,7 +65,7 @@ function blocker(v::AbstractVector{T}) where T <: Complex
     P = typeof(zero(T)/1)
     new_v = StructArray{P}(undef,(length(v)÷2))
     for i  in 1:length(v)÷2
-        new_v[i] = (v[2i-1]+v[2i])/2
+        @inbounds new_v[i] = (v[2i-1]+v[2i])/2
     end
     return new_v
 end
