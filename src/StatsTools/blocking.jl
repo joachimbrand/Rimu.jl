972b31a2b7444898ab00d0be1cc46e4a3a8dbeb4
Joachim Brand <j.brand@massey.ac.nz>
2021-05-01 00:19:28 +1200

    fix errors

--- src/StatsTools/blocking.jl
+++ src/StatsTools/blocking.jl
@@ -153,14 +153,14 @@ function blocker(v::AbstractVector{T}) where T
     end
     return new_v
 end
-function blocker(v::AbstractVector{T}) where T <: Complex
-    P = typeof(zero(T)/1)
-    new_v = StructArray{P}(undef,(length(v)÷2))
-    for i  in 1:length(v)÷2
-        @inbounds new_v[i] = (v[2i-1]+v[2i])/2
-    end
-    return new_v
-end
+# function blocker(v::AbstractVector{T}) where T <: Complex
+#     P = typeof(zero(T)/1)
+#     new_v = StructArray{P}(undef,(length(v)÷2))
+#     for i  in 1:length(v)÷2
+#         @inbounds new_v[i] = (v[2i-1]+v[2i])/2
+#     end
+#     return new_v
+# end
 
 # """
 #     blocker!(v::Vector)
