5d562fc2968ad806027dbb4e4098adc801a1bbee
Joachim Brand <j.brand@massey.ac.nz>
2021-05-01 00:43:50 +1200

    doc string change

--- src/StatsTools/blocking.jl
+++ src/StatsTools/blocking.jl
@@ -31,6 +31,7 @@ end
     cov(r::BlockingResult{<:Complex})
 Return the covariance matrix of the multivariate normal distribution approximating
 the uncertainty of the blocking result `r` of a complex time series.
+See (https://en.wikipedia.org/wiki/Complex_normal_distribution).
 """
 function Statistics.cov(r::BlockingResult{<:Complex})
     v_xx = real(r.err^2 + r.p_cov)/2
