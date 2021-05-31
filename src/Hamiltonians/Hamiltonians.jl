f6ae90df9d03c536abfe7e2e2cd4bb274025041c
Mingrui Yang <mingrui.yang@outlook.com>
2021-05-31 12:26:29 +1200

    export TwoBodyCorrelation

--- src/Hamiltonians/Hamiltonians.jl
+++ src/Hamiltonians/Hamiltonians.jl
@@ -23,6 +23,7 @@ export BoseHubbardMom1D2C, BoseHubbardReal1D2C
 export GutzwillerSampling, GuidingVectorSampling
 
 export BoseHubbardReal1D, ExtendedBHReal1D, BoseHubbardMom1D
+export TwoBodyCorrelation
 
 include("abstract.jl")
 include("offdiagonals.jl")
