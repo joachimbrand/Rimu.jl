7205b863dea68e4038d64a602e23584e879653b0
mtsch <matijacufar@gmail.com>
2021-06-02 15:12:57 +1200

    Add QMCState serialization capabilities

--- src/RimuIO.jl
+++ src/RimuIO.jl
@@ -7,7 +7,9 @@ Provides convenience functions:
 """
 module RimuIO
 
-using Arrow, DataFrames
+using ..DictVectors
+
+using Arrow, DataFrames, JLSO
 
 export save_df, load_df
 
@@ -29,5 +31,41 @@ save_df(filename, df::DataFrame) = Arrow.write(filename, df)
 Load Arrow file into dataframe.
 """
 load_df(filename) = DataFrame(Arrow.Table(filename))
+
+function serialize_state(filename, state)
+    # Extract pairs from DVecs for better storage
+    temp_storage = map(r -> r.v, state.replicas)
+    dvec_pairs = map(r -> collect(pairs(storage(r.v))), state.replicas)
+
+    # Empty working memory and replace vectors with empty ones.
+    for r in state.replicas
+        if r.w isa Tuple
+            map(empty!, r.w)
+        else
+            empty!(r.w)
+        end
+        r.v = empty(r.v)
+    end
+
+    # Serialize both
+    JLSO.save(filename, :state => state, :dvec_pairs => dvec_pairs)
+
+    # Regenerate the state
+    for (r, v) in zip(state.replicas, temp_storage)
+        r.v = v
+    end
+    return filename
+end
+
+function deserialize_state(filename)
+    d = JLSO.load(filename)
+    state = d[:state]
+    for (r, ps) in zip(state.replicas, d[:dvec_pairs])
+        for (k, v) in ps
+            storage(r.v)[k] = v
+        end
+    end
+    return state
+end
 
 end # module RimuIO
