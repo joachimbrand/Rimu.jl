d10bf3ba84a575ee5e5dfe19080b79466401b300
mtsch <matijacufar@gmail.com>
2021-06-15 17:55:08 +1200

    Update benchmark suite (#87)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,13 +1,31 @@
 using Rimu
 using BenchmarkTools
 
-const SUITE = BenchmarkGroup()
-const SKIP = []
+const SUITE = @benchmarkset "Rimu" begin
+    @case "(10, 20) Mom space with projected energy and initiator" begin
+        add = BoseFS(ntuple(i -> ifelse(i == 10, 10, 0), 20))
+        ham = HubbardMom1D(add, u=6.0)
+        dv = InitiatorDVec(add => 1.0; style=IsDynamicSemistochastic())
+        r_strat = EveryTimeStep(projector=copy(dv))
+        s_strat = DoubleLogUpdate(targetwalkers=20_000)
 
-for file in readdir(@__DIR__)
-    if file in SKIP
-        @warn "skipping $file"
-    elseif startswith(file, "bench_") && endswith(file, ".jl")
-        SUITE[file[(length("bench_") + 1):(end - length(".jl"))]] = include(file)
+        lomc!(ham, dv; s_strat, r_strat, dτ=1e-4, laststep=8000)
+    end
+    @case "(4+1, 11) 2C Mom space with G2Correlators" begin
+        add = BoseFS2C(ntuple(i -> ifelse(i == 5, 4, 0), 11), ntuple(==(5), 11))
+        ham = BoseHubbardMom1D2C(add, v=0.1)
+        dv = DVec(add => 1.0; style=IsDynamicSemistochastic())
+        s_strat = DoubleLogUpdate(targetwalkers=10_000)
+        replica = AllOverlaps(2, ntuple(i -> G2Correlator(i - 1), 7))
+
+        lomc!(ham, dv; s_strat, replica, laststep=4000)
+    end
+    @case "(50, 50) Real space" begin
+        add = nearUniform(BoseFS{50,50})
+        ham = HubbardReal1D(add, u=6.0)
+        dv = DVec(add => 1.0; style=IsDynamicSemistochastic())
+        s_strat = DoubleLogUpdate(targetwalkers=50_000)
+
+        lomc!(ham, dv; s_strat, dτ=1e-4, laststep=4000)
     end
 end
