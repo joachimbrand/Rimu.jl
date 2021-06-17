0f99d1752748e6487a0d68959b1e1c1eb66a6e01
mtsch <matijacufar@gmail.com>
2021-06-18 09:34:39 +1200

    Check in tune.json, make sure each test gets run twice (#88)

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -9,8 +9,8 @@ const SUITE = @benchmarkset "Rimu" begin
         post_step = ProjectedEnergy(ham, dv)
         s_strat = DoubleLogUpdate(targetwalkers=20_000)
 
-        lomc!(ham, dv; s_strat, post_step, dτ=1e-4, laststep=8000)
-    end
+        lomc!(ham, dv; s_strat, post_step, dτ=1e-4, laststep=4000)
+    end seconds=150
     @case "(4+1, 11) 2C Mom space with G2Correlators" begin
         add = BoseFS2C(ntuple(i -> ifelse(i == 5, 4, 0), 11), ntuple(==(5), 11))
         ham = BoseHubbardMom1D2C(add, v=0.1)
@@ -18,14 +18,14 @@ const SUITE = @benchmarkset "Rimu" begin
         s_strat = DoubleLogUpdate(targetwalkers=10_000)
         replica = AllOverlaps(2, ntuple(i -> G2Correlator(i - 1), 7))
 
-        lomc!(ham, dv; s_strat, replica, laststep=4000)
-    end
+        lomc!(ham, dv; s_strat, replica, laststep=2000)
+    end seconds=150
     @case "(50, 50) Real space" begin
         add = nearUniform(BoseFS{50,50})
         ham = HubbardReal1D(add, u=6.0)
         dv = DVec(add => 1.0; style=IsDynamicSemistochastic())
         s_strat = DoubleLogUpdate(targetwalkers=50_000)
 
-        lomc!(ham, dv; s_strat, dτ=1e-4, laststep=4000)
-    end
+        lomc!(ham, dv; s_strat, dτ=1e-4, laststep=2000)
+    end seconds=150
 end
