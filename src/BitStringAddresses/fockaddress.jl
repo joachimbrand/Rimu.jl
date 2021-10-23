3c2a310f7815da06225d3d00048429a39e99f0fc
mtsch <matijacufar@gmail.com>
2021-10-23 16:27:30 +1300

    Fix `OccupiedModeMap` docstring

--- src/BitStringAddresses/fockaddress.jl
+++ src/BitStringAddresses/fockaddress.jl
@@ -198,7 +198,7 @@ julia> b = BoseFS((10, 0, 0, 0, 2, 0, 1))
 BoseFS{13,7}((10, 0, 0, 0, 2, 0, 1))
 
 julia> mb = OccupiedModeMap(b)
-3-element OccupiedModeMap{13, Rimu.BitStringAddresses.BoseFSIndex}:
+3-element OccupiedModeMap{7, Rimu.BitStringAddresses.BoseFSIndex}:
  BoseFSIndex(occnum=10, mode=1, offset=0)
  BoseFSIndex(occnum=2, mode=5, offset=14)
  BoseFSIndex(occnum=1, mode=7, offset=18)
