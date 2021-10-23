becbf1e47b4bc288ea2667ef72d62ef0bb8c3acf
Joachim Brand <j.brand@massey.ac.nz>
2021-10-23 23:09:45 +1300

    fix docstring of find_mode

--- src/BitStringAddresses/fockaddress.jl
+++ src/BitStringAddresses/fockaddress.jl
@@ -56,14 +56,14 @@ num_components(::Type{<:SingleComponentFockAddress}) = 1
     find_mode(::SingleComponentFockAddress, i)
 
 Find the `i`-th mode in address. Returns [`BoseFSIndex`](@ref) for [`BoseFS`](@ref), and
-an integer index for [`FermiFS`](@ref). Does not check bounds.
+an integer index for [`FermiFS`](@ref). Can work on a tuple of modes. Does not check bounds.
 
 ```jldoctest
 julia> find_mode(BoseFS((1, 0, 2)), 2)
 BoseFSIndex(occnum=0, mode=2, offset=2)
 
-julia> find_mode(FermiFS((1, 1, 1, 0)), 2)
-FermiFSIndex(occnum=1, mode=2)
+julia> find_mode(FermiFS((1, 1, 1, 0)), (2,3))
+(FermiFSIndex(occnum=1, mode=2), FermiFSIndex(occnum=1, mode=3))
 ```
 """
 find_mode
