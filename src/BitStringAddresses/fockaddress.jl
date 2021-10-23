5dab1df482ed8a172698c06d3246441be82d03b8
Joachim Brand <j.brand@massey.ac.nz>
2021-10-24 00:19:04 +1300

    docstring of find_occupied_mode

--- src/BitStringAddresses/fockaddress.jl
+++ src/BitStringAddresses/fockaddress.jl
@@ -73,19 +73,24 @@ find_mode
 
 """
     find_occupied_mode(::SingleComponentFockAddress, k)
+    find_occupied_mode(::BoseFS, k, [n])
 
-Find the `k`-th occupied mode in address. Returns [`BoseFSIndex`](@ref) for
-[`BoseFS`](@ref), and an integer for [`FermiFS`](@ref). When unsuccessful it
+Find the `k`-th occupied mode in address (with at least `n` particles).
+Returns [`BoseFSIndex`](@ref) for [`BoseFS`](@ref), and an integer for [`FermiFS`](@ref).
+When unsuccessful it
 returns zero.
 
 # Example
 
 ```jldoctest
-julia> find_occupied_mode(BoseFS((1, 0, 2)), 2)
-BoseFSIndex(occnum=2, mode=3, offset=3)
-
 julia> find_occupied_mode(FermiFS((1, 1, 1, 0)), 2)
 FermiFSIndex(occnum=1, mode=2)
+
+julia> find_occupied_mode(BoseFS((1, 0, 2)), 1)
+BoseFSIndex(occnum=1, mode=1, offset=0)
+
+julia> find_occupied_mode(BoseFS((1, 0, 2)), 1, 2)
+BoseFSIndex(occnum=2, mode=3, offset=3)
 ```
 
 See also [`occupied_modes`](@ref), [`OccupiedModeMap`](@ref),
