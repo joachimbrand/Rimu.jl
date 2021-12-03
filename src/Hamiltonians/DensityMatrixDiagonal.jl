f110f0eab690afc568ac9a10306617b42509d167
mtsch <matijacufar@gmail.com>
2021-12-03 13:32:16 +1300

    DensityMatrixDiagonal

---
+++ src/Hamiltonians/DensityMatrixDiagonal.jl
@@ -0,0 +1,28 @@
+struct DensityMatrixDiagonal{C} <: AbstractHamiltonian{Float64}
+    mode::Int
+end
+function DensityMatrixDiagonal(; mode, component=1)
+    if component ≤ 0
+        throw(ArgumentError("Invalid component `$component`"))
+    end
+    return DensityMatrixDiagonal{component}(mode)
+end
+
+function diagonal_element(spd::DensityMatrixDiagonal{C}, add::CompositeFS) where {C}
+    comp = add.components[C]
+    return float(find_mode(comp, spd.mode).occnum)
+end
+function diagonal_element(spd::DensityMatrixDiagonal{1}, add::BoseFS2C)
+    comp = add.bsa
+    return float(find_mode(comp, spd.mode).occnum)
+end
+function diagonal_element(spd::DensityMatrixDiagonal{2}, add::BoseFS2C)
+    comp = add.bsb
+    return float(find_mode(comp, spd.mode).occnum)
+end
+function diagonal_element(spd::DensityMatrixDiagonal{1}, add::SingleComponentFockAddress)
+    return float(find_mode(add, spd.mode).occnum)
+end
+
+num_offdiagonals(spd::DensityMatrixDiagonal, _) = 0
+LOStructure(::Type{<:DensityMatrixDiagonal}) = IsHermitian()
