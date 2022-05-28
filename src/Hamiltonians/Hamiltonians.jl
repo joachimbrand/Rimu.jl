76867a30d2bbb34c932cba1d615826fca01131a7
Chris Bradly <christof.bradly@gmail.com>
2022-05-28 17:57:16 +1200

    export G2

--- src/Hamiltonians/Hamiltonians.jl
+++ src/Hamiltonians/Hamiltonians.jl
@@ -52,7 +52,7 @@ export TimeReversalSymmetry
 export Transcorrelated1D
 export hubbard_dispersion, continuum_dispersion
 
-export G2Correlator, DensityMatrixDiagonal, Momentum
+export G2Correlator, G2RealCorrelator, DensityMatrixDiagonal, Momentum
 
 export LatticeGeometry, PeriodicBoundaries, HardwallBoundaries, LadderBoundaries
 export num_neighbours, neighbour_site
