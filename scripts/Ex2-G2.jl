85c914be21fa6abe13bd8097781f608ad17b8f11
Chris Bradly <christof.bradly@gmail.com>
2022-09-01 15:55:50 +1200

    latex and tests

--- scripts/Ex2-G2.jl
+++ scripts/Ex2-G2.jl
@@ -11,14 +11,13 @@
 
 using Rimu
 using DataFrames
-using Printf
 
 # We use the same Hamiltonian as the previous examples,
 # a Bose-Hubbard model with 6 particles in 6 sites, with
 # strong interactions (we expect a Mott insulating state).
 m = n = 6
 aIni = near_uniform(BoseFS{n,m})
-Ĥ = HubbardReal1D(aIni; u = 6.0, t = 1.0)
+H = HubbardReal1D(aIni; u = 6.0, t = 1.0)
 
 # Now we define the operators for the observables we wish to calculate
 dvals = 0:m-1
@@ -27,11 +26,11 @@ G2list = ([G2RealCorrelator(d) for d in dvals]...,)
 # `AbstractHamiltonian`, but with less functionality than a full Hamiltonian.
 # It calculates the two-body correlation function on a lattice
 # ```math
-#     \\hat{G}^{(2)}(d) = \\frac{1}{M} \\sum_i^M \\hat{n}_i (\\hat{n}_{i+d} - \\delta_{0d}).
+#     \hat{G}^{(2)}(d) = \frac{1}{M} \sum_i^M \hat{n}_i (\hat{n}_{i+d} - \delta_{0d}).
 # ```
 # with normalisation
 # ```math
-#     \\sum_{d=0}^{M-1} \\langle \\hat{G}^{(2)}(d) \\rangle = \\frac{N (N-1)}{M}.
+#     \sum_{d=0}^{M-1} \langle \hat{G}^{(2)}(d) \rangle = \frac{N (N-1)}{M}.
 # ```
 
 # Observables are calculated using the "replica trick" which runs independent 
@@ -42,41 +41,24 @@ G2list = ([G2RealCorrelator(d) for d in dvals]...,)
 num_reps = 3
 replica = AllOverlaps(num_reps; operator = G2list)
 
-
-# We will deliberately use a small number of walkers to amplify
-# the effect of the statistical tools that we will test.
-targetwalkers = 100
-# We need a reasonable number of timesteps to get good statistics
+# We need a reasonable number of timesteps to get good statistics, and we are running
+# multiple replicas, so we will only use a small number of walkers
 steps_equilibrate = 1_000
 steps_measure = 5_000
+targetwalkers = 100
 
-
-# Other FCIQMC parameters are the same as before
+# Other FCIQMC parameters and strategies are the same as before
 dτ = 0.001
 reporting_interval = 1
 svec = DVec(aIni => 1)
 seedCRNG!(17)
-
-# Other FCIQMC strategies are the same as before
-
-# Passing dτ and total number of time steps into params:
 params = RunTillLastStep(dτ = dτ, laststep = steps_equilibrate + steps_measure)
-# Strategy for updating the shift:
 s_strat = DoubleLogUpdate(targetwalkers = targetwalkers, ζ = 0.08)
-# Strategy for reporting info:
 r_strat = ReportDFAndInfo(reporting_interval = reporting_interval, info_interval = 100)
-# Strategy for updating dτ:
-τ_strat = ConstantTimeStep()
+τ_strat = ConstantTimeStep();
 
-# Print out info about what we are doing:
-println("Finding ground state for:")
-println(Ĥ)
-println("Strategies for run:")
-println(params, s_strat)
-println(τ_strat)
-
-# Finally, we can start the main FCIQMC loop:
-df, state = lomc!(Ĥ,svec;
+# Now we run the main FCIQMC loop:
+df, state = lomc!(H, svec;
             params,
             laststep = steps_equilibrate + steps_measure,
             s_strat,
@@ -84,7 +66,7 @@ df, state = lomc!(Ĥ,svec;
             τ_strat,
             replica,
             threading = false, # only for reproducible runs
-)
+);
 
 # The output `DataFrame` has FCIQMC statistics for each replica 
 # (e.g. shift) 
@@ -97,7 +79,7 @@ println(filter(contains("Op"), names(df)))
 # The vector-vector and operator overlaps go into calculating the Rayleigh quotient 
 # for an observable
 # ```math
-#     \\langle \\hat{G}^{(2)}(d) \\rangle = \\frac{\\sum_{a<b} \\mathbf{c}_a^\\dagger \\hat{G}^{(2)}(d) \\mathbf{c}_b}{\\sum_{a<b} \\mathbf{c}_a^\\dagger \\cdot \\mathbf{c}_b \\}
+#     \langle \hat{G}^{(2)}(d) \rangle = \frac{\sum_{a<b} \mathbf{c}_a^\dagger \cdot \hat{G}^{(2)}(d) \cdot \mathbf{c}_b}{\sum_{a<b} \mathbf{c}_a^\dagger \cdot \mathbf{c}_b }
 # ```
 # The sum over all replica pairs (a,b), especially in the denominator, helps to avoid 
 # errors from poor sampling if the number of walkers is too low.
@@ -110,7 +92,7 @@ println(filter(contains("Op"), names(df)))
 println("Two-body correlator from $num_reps replicas:")
 for d in dvals
     r = rayleigh_replica_estimator(df; op_name = "Op$(d+1)", skip=steps_equilibrate)
-    println("   G2($d) = $(@sprintf("%.3f", r.f)) ± $(@sprintf("%.3f", r.σ_f))")
+    println("   G2($d) = $(r.f) ± $(r.σ_f)")
 end
 
 # As expected, the onsite correlation at ``d=0`` is low since this is 
@@ -122,7 +104,7 @@ end
 println("Shift energy from $num_reps replicas:")
 for i in 1:num_reps
     se = shift_estimator(df; shift="shift_$i", skip=steps_equilibrate)
-    println("   Replica $i: $(@sprintf("%.2f", se.mean)) ± $(@sprintf("%.2f", se.err))")
+    println("   Replica $i: $(se.mean) ± $(se.err)")
 end
 
 # Finished !
