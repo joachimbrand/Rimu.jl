44fc507dff6ec3246a8e7cac1dcc992051dc6508
Chris Bradly <christof.bradly@gmail.com>
2022-09-02 15:37:38 +1200

    function with warning

--- docs/make.jl
+++ docs/make.jl
@@ -11,14 +11,25 @@ EXAMPLES_FILES = filter(endswith(".jl"), readdir(EXAMPLES_INPUT))
 EXAMPLES_PAIRS = Pair{String,String}[]
 EXAMPLES_NUMS = Int[]
 
+function parse_header(filename::String)
+    try
+        header = split(filter(
+                    contains(r"# Example [0-9]+:"), 
+                    readlines(open(filename))
+                )[1], ":")
+        n = parse(Int, match(r"[0-9]+", header[1]).match)
+        t = lstrip(header[2])
+        return n, t
+    catch
+        @warn "couldn't parse example file for title in \"" * filename * "\""
+        return 100, "BAD_EXAMPLE"
+    end
+end
+
 for fn in EXAMPLES_FILES
     fnmd_full = Literate.markdown(joinpath(EXAMPLES_INPUT, fn), EXAMPLES_OUTPUT; documenter=true)
-    header = split(filter(
-                    contains(r"# Example [0-9]+:"), 
-                    readlines(open(fnmd_full))
-                )[1], ":")
-    push!(EXAMPLES_NUMS, parse(Int, match(r"[0-9]+", header[1]).match))
-    margintitle = lstrip(header[2])
+    ex_num, margintitle = parse_header(fnmd_full)
+    push!(EXAMPLES_NUMS, ex_num)    
     fnmd = fn[1:end-2]*"md"     # full path does not work
     push!(EXAMPLES_PAIRS, margintitle => joinpath("generated", fnmd))
 end
