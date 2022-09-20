9e60c54b4d62236665f5ad95e140f49d04d81c96
Ray Yang <mingrui.yang@outlook.com>
2022-09-21 11:19:15 +1200

    more text

--- scripts/Impurity-example.jl
+++ scripts/Impurity-example.jl
@@ -4,9 +4,19 @@
 # impurity coupled with a one-dimensional Bose gas. We will be using MPI parallelisation
 # as such calculations are typically expensive. 
 
+# The aim of this example is to showcase how a two-component Hamiltonian in momentum-space can be set up, 
+# as well as how a multi-stage FCIQMC can be run.
+
+# A detailed description of the physical system and the physics behind the model can be found 
+# at our published paper (open access) "Polaron-Depleton Transition in the Yrast Excitations of a One-Dimensional 
+# Bose Gas with a Mobile Impurity", M. Yang, M. Čufar, E. Pahl, J. Brand, 
+# [*Condens. Matter* **7**, 15 (2022)](https://www.mdpi.com/2410-3896/7/1/15).
+
 # A runnable script for this example is located 
 # [here](https://github.com/joachimbrand/Rimu.jl/blob/develop/scripts/Impurity-example.jl).
 # Run it with `mpirun -np [# of CPUs] julia Impurity-example.jl`.
+
+### Setup
 
 # Firstly, we load all needed modules.
 # `Rimu` and `Rimu.RMPI` for parallel FCIQMC calculation, and `DataFrames` for output
@@ -86,13 +96,13 @@ ham2 = BoseHubbardMom1D2C(aIni;ta=T,tb=T,ua=U,v=V2,dispersion=continuum_dispersi
 # Now we can setup the Monte Carlo parameters
 steps_warmup = 10_000 # number of QMC steps running with `ham2`
 steps_equilibrate = 10_000 # number of QMC steps running with the real `ham`
-steps_final = 1_000 # number of QMC steps running with G2 correlators, very slow, be caution!
+steps_final = 10_000 # number of QMC steps running with G2 correlators, very slow, be caution!
 tw = 1_000 # number of walkers
 
 # Specifying the shift strategy:
 s_strat = DoubleLogUpdateAfterTargetWalkers(targetwalkers = tw)
 # We use very small time-step size and high starting value of shift
-params = RunTillLastStep(step = 0, dτ = 0.00001, laststep = steps_warmup,shift = 3_000.0)
+params = RunTillLastStep(step = 0, dτ = 0.00001, laststep = steps_warmup,shift = 200.0)
 # As we only use the secondary Hamiltonian `ham2` to generate a staring vector, we don't have to
 # save any data in this stage
 r_strat = ReportDFAndInfo(reporting_interval = 1_000, info_interval = 1_000, writeinfo = is_mpi_root())
@@ -100,10 +110,17 @@ r_strat = ReportDFAndInfo(reporting_interval = 1_000, info_interval = 1_000, wri
 # Wrapping `dv` for MPI:
 dv = MPIData(init_dv(P,m,na))
 
+# Let's have a look of the starting vector:
+@mpi_root @show dv
+
+### Stage 1: Running the "dummy" Hamiltonian
+
 # Now we run FCIQMC with `lomc!()` and track the elapsed time. 
 # Both `df` and `state` will be overwritten late with the "real" data
 el = @elapsed df, state = lomc!(ham2, dv; params, s_strat, r_strat,)
 @mpi_root @info "Initial fciqmc completed in $(el) seconds."
+
+### Stage 2: Running the real Hamiltonian with replica but no observables
 
 # New we are ready to run the real Hamiltonian, here we redefine some variables for saving outputs.
 # We save the Monte Carlo data every 1000 steps.
@@ -119,20 +136,25 @@ el2 = @elapsed df, state = lomc!(ham,dv; params, s_strat, r_strat, replica=AllOv
 				 laststep=(steps_equilibrate+steps_warmup))
 @mpi_root @info "Replica fciqmc completed in $(el2) seconds."
 
+### Stage 3: Running the real Hamiltonian with replica with observables
 
 # We now at the last stage of the calculation, doing replica FCIQMC with a serious of 
 # G2 correlators with distance `d` from `0` to `m`. See [`G2Correlator`](@ref).
-# Save data every 10 steps as they are slow and you don't want to loose too much data if
-# the job stops before finishes.
+# Here we save data every 1000 steps, but using a smaller `chunk_size` like 10 or even 1
+# is highly recommended as replica FCIQMC with many observables being calculated are very 
+# expensive and you don't want to loose too much data if the job stops before finishes.
 r_strat = ReportToFile(
 		       save_if = is_mpi_root(),
 		       filename = "mpi_df_g2_$(η)_$(P).arrow",
-		       chunk_size = 10,
+		       chunk_size = 1000,
                return_df = true # change it to `false` when running the real job
 		       )
 
 # Setting up a tuple of G2 correlators:
 g = Tuple(G2Correlator.(0:m))
+# By default, for a two-component system the cross-component G2 operators are set up.
+# If you want to calculate the correlations within a single component, `G2Correlator(d,:first)` 
+# or `G2Correlator(d,:second)` can be called based on your choice.
 
 # Carry over information from the previous stage and set up a new `QMCState`:
 new_state = Rimu.QMCState(
@@ -142,4 +164,15 @@ new_state = Rimu.QMCState(
 # The final stage 
 el3 = @elapsed df2, state2 = lomc!(new_state;laststep=(steps_equilibrate+steps_warmup+steps_final))
 @mpi_root @info "Replica fciqmc with G2 completed in $(el3) seconds."
-@mpi_root println("Done!")
+@mpi_root println("MPI run finished!")
+
+# Typically, one should not include any analyses when using MPI, as they will be calculated multiple
+# time unless you put the `@mpi_root` macro everywhere.
+# But here, let's have a look of the calculated G2 correlations:
+@mpi_root println("Two-body correlator from 2 replicas:")
+@mpi_root for d in 0:m
+    r = rayleigh_replica_estimator(df2; op_name = "Op$(d+1)", skip=1_000)
+    println("   G2($d) = $(r.f) ± $(r.σ_f)")
+end
+# A symmetry between `G2(d)` and `G2(m-d)` can be observed above, which is the expected outcome due 
+# the periodic boundary conditions. 
