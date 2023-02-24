1e045c94c4f1ac401420369fc9d45a41a1f748b4
mtsch <matijacufar@gmail.com>
2023-02-25 00:22:50 +1300

    Change remaining `w` to `v` in `walkernumber` docs

--- src/DictVectors/abstractdvec.jl
+++ src/DictVectors/abstractdvec.jl
@@ -204,7 +204,7 @@ LinearAlgebra.normalize(v::AbstractDVec, p::Real=2) = normalize!(copy(v), p)
 Compute the number of walkers in `v`. It is used for updating the shift. Overload this
 function for modifying population control.
 
-In most cases `walkernumber(w)` is identical to `norm(w,1)`. For `AbstractDVec`s with
+In most cases `walkernumber(v)` is identical to `norm(v, 1)`. For `AbstractDVec`s with
 complex coefficients it reports the one norm separately for the real and the imaginary part
 as a `ComplexF64`. See [`Norm1ProjectorPPop`](@ref).
 """
