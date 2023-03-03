2f457eb92418cc4fdb7191c08d5eceb40767d1d1
Joachim Brand <joachim.brand@gmail.com>
2023-03-03 22:17:01 +1300

    small tweak

--- src/RMPI/mpidata.jl
+++ src/RMPI/mpidata.jl
@@ -99,13 +99,13 @@ end
 # Replacing it by `+` is necessary for non-Intel architectures due to a limitation of
 # MPI.jl. On Intel processors, it might be more perfomant.
 # see https://github.com/JuliaParallel/MPI.jl/issues/404
-function Base.mapreduce(f, ::typeof(Base.add_sum), it::MPIDataIterator; kwargs...)
-    res = mapreduce(f, +, it.iter; kwargs...)
+function Base.mapreduce(f, op::typeof(Base.add_sum), it::MPIDataIterator; kwargs...)
+    res = mapreduce(f, op, it.iter; kwargs...)
     return MPI.Allreduce(res, +, it.data.comm)
 end
 
-function Base.mapreduce(f, ::typeof(Base.mul_prod), it::MPIDataIterator; kwargs...)
-    res = mapreduce(f, *, it.iter; kwargs...)
+function Base.mapreduce(f, op::typeof(Base.mul_prod), it::MPIDataIterator; kwargs...)
+    res = mapreduce(f, op, it.iter; kwargs...)
     return MPI.Allreduce(res, *, it.data.comm)
 end
 
