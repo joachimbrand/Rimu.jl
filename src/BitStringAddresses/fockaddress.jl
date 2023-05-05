6d6d6e2d0568973269a710c980f9c66363509f36
Chris Bradly <christof.bradly@gmail.com>
2023-05-05 17:28:43 +1200

    fix jldoctest

--- src/BitStringAddresses/fockaddress.jl
+++ src/BitStringAddresses/fockaddress.jl
@@ -666,6 +666,7 @@ This is an eager iterator whose elements are a tuple of particle indices that
 can be given to `excitation`
 
 # Example
+
 ```jldoctest
 julia> addr = BoseFS((10, 0, 0, 0, 2, 0, 1))
 BoseFS{13,7}((10, 0, 0, 0, 2, 0, 1))
@@ -680,7 +681,7 @@ julia> pairs = OccupiedModeMap(addr)
 
 julia> excitation(addr, pairs[2], pairs[4])
 (BoseFS{13,7}((9, 0, 0, 0, 4, 0, 0)), 10.954451150103322)
- ```
+```
 
 See also [`OccupiedModeMap`](@ref).
 """
