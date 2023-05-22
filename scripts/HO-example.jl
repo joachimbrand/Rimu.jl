d8198b98bc2234f9c8ee105d11cd162ab4398e77
Chris Bradly <christof.bradly@gmail.com>
2023-05-22 14:45:57 +1200

    simplify setup and tweak wording

--- scripts/HO-example.jl
+++ scripts/HO-example.jl
@@ -1,52 +1,50 @@
 # # Example 5: Harmonic oscillator
 
-# This is an example calculation of the harmonic oscillator with
-# contact interactions that are treated with first-order perturbation
-# theory.
+# `Rimu` can also handle non-lattice systems. This example shows how a harmonic 
+# oscillator Hamiltonian is implemented, and how to calculate blocks of energy 
+# eigenstates in the Hamiltonian and angular momentum eigenstates.
 
 # A runnable script for this example is located
 # [here](https://github.com/joachimbrand/Rimu.jl/blob/develop/scripts/HO-example.jl).
 # Run it with `julia HO-example.jl`.
 
-# Firstly, we load all needed modules.
+# First, load all needed modules.
 using Rimu
 using DataFrames
 using LinearAlgebra
 
-# First we define the system size for two particles in a 2D harmonic oscillator
-# allowing `M` levels above the ground state in each dimension.
+# Define the system size for ``N=2`` particles in a 2D harmonic oscillator
+# allowing ``M=4`` levels in each dimension, including the groundstate.
 N = 2
-D = 2
-M = 3
-S = ntuple(_ -> M + 1, D)
-P = prod(S)
+M = 4
 
-# The tuple `S` defines a grid of harmonic oscillator states in a Cartesian
-# basis (`P` is the total number of single-particle states), in this case 
-# ``n_x,n_y=0,1,\ldots,M``.
+# Use a tuple `S` to define the range of harmonic oscillator states in a Cartesian
+# basis, in this isotropic case ``n_x,n_y=0,1,\ldots,M-1``.
+S = (M, M)
+
 # In `Rimu` the ``N``-particle states are still stored as Fock states.
+P = prod(S)
+addr = BoseFS(P, M => N)
 # Use the utility function [`fock_to_cart`](@ref) to convert a Fock address to 
 # human-readable Cartesian quantum numbers for inspection.
-addr = BoseFS(P, M + 1 => N)
 fock_to_cart(addr, S)
-# The output shows that all ``N`` particles are in single-particle state ``n_x=M, n_y=0``.
+# The output shows that all ``N`` particles are in single-particle state ``n_x=M-1, n_y=0``.
 
 # The harmonic oscillator Hamiltonian [`HOCartesianEnergyConserved`](@ref) handles 
 # contact interactions with
 # first-order perturbation theory, so the matrix representation will block
-# according to the non-interacting energy of the basis states. Hence the first task is 
+# according to the non-interacting energy of the basis states. The first task is 
 # to find all blocks of basis states with the same energy. The strength of the 
 # interaction is not relevant at this point, just that it is non-zero.
-# We use a dummy groundstate address to build the Hamiltonian 
+# Use a dummy groundstate address to build the Hamiltonian 
 H = HOCartesianEnergyConserved(BoseFS(P, 1 => N); S)
-
-# and then a utility function [`get_all_blocks`](@ref) to find all blocks up
-# to the maximum single-particle energy, which is `M` levels above the groundstate. 
-# (Each level is a jump of ``\hbar \omega``.)
-# This works by looping over all 
-# possible states with `N` particles in Cartesian states defined by `S`.
-E0 = N*D/2
-block_df = get_all_blocks(H; max_energy = E0 + M)
+# and then a utility function [`get_all_blocks`](@ref) to find all blocks. The blocks 
+# are found by looping over all possible states with `N` particles in Cartesian states 
+# defined by `S`. Note that this will only work for total energy 
+# up to the maximum accessible by a single particle.
+# The ``N``-particle groundstate energy for a 2D harmonic oscillator is 
+# ``E_0 = N \hbar \omega`` and the maximum single-particle energy is ``E = (E_0 + M - 1) \hbar \omega``.
+block_df = get_all_blocks(H; max_energy = N + M - 1)
 
 # This outputs a list of blocks in `H` indexed by the noninteracting energy
 # of all states in the block, and a single address that can be used to 
@@ -57,7 +55,7 @@ E = block_df[7,:block_E0]
 basis1 = build_basis(H, addr1)
 map(b -> Hamiltonians.noninteracting_energy(H, b), basis1)
 
-# There are ``2^{D-1}`` blocks at each energy level (except the groundstate), which 
+# There are two blocks at each energy level (except the groundstate), which 
 # are different due to parity conservation, which is the only other symmetry in the 
 # Cartesian harmonic oscillator.
 # The basis of this other block is different 
@@ -67,24 +65,24 @@ basis1 ≠ basis2
 # but its basis elements have the same energy
 map(b -> Hamiltonians.noninteracting_energy(H, b), basis2)
 
-# However, since we have defined an isotropic harmonic oscillator, we
-# should be able to build simultaneous eigenstates of the angular momentum operator
+# However, since this system is an isotropic harmonic oscillator, it is possible 
+# to build simultaneous eigenstates of the angular momentum operator
 # ``L_z``, implemented with [`AxialAngularMomentumHO`](@ref)
 Lz = AxialAngularMomentumHO(S)
 
-# ``L_z`` does not conserve parity so we need both blocks. First combine the bases 
+# ``L_z`` does not conserve parity so both blocks are required. First combine the bases 
 # of each block and convert to `DVecs`
 dvs = map(b -> DVec(b => 1.0), vcat(basis1, basis2));
 # and then compute overlaps for the matrix elements of ``L_z``
 Lz_mat = [dot(v, Lz, w) for v in dvs, w in dvs]
 
-# By diagonalising this matrix we obtain states of energy `E` and well-defined angular
+# By diagonalising this matrix the eigenstate have energy `E` and well-defined angular
 # momentum
 Lz_vals, Lz_vecs = eigen(Lz_mat)
 
-# Finally we can consider the effect of interactions by looking at how states 
-# in a single block are perturbed. We are interested in the energy shift due to 
-# the interaction so we rebuild the Hamiltonian without the non-interacting energy
+# Finally, consider the effect of interactions by looking at how states 
+# in a single block are perturbed. Only the energy shift due to the interaction is 
+# relevant so now rebuild the Hamiltonian without the non-interacting energy
 Hint = HOCartesianEnergyConserved(addr1; S, interaction_only = true)
 ΔE = eigvals(Matrix(Hint, addr1))
 
@@ -106,7 +104,7 @@ Hint = HOCartesianEnergyConserved(addr1; S, interaction_only = true)
 # Finished!
 
 using Test                                      #hide
-@test nrow(block_df) == M*2^(D-1) + 1           #hide
-@test E == E0 + M                               #hide
+@test nrow(block_df) == 2*(M-1) + 1             #hide
+@test E == E0 + M - 1                           #hide
 @test Lz_vals ≈ [-3,-3,-1,-1,-1,1,1,1,3,3]      #hide
 nothing                                         #hide
