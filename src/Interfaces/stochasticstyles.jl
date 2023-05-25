a4bebc1dde512afdd58be95b930f8af208b86604
mtsch <matijacufar@gmail.com>
2023-05-25 19:33:05 +1200

    fix docstring code

--- src/Interfaces/stochasticstyles.jl
+++ src/Interfaces/stochasticstyles.jl
@@ -115,12 +115,13 @@ step_stats(v) = step_stats(StochasticStyle(v))
 """
     apply_column!(v, op, addr, num, boost=1) -> stats::Tuple
 
-Apply the product of column `addr` of the operator `op` and the scalar `num` to the 
-vector `v` according to the [`StochasticStyle`](@ref) of `v`. By expectation value this 
-should be equivalent to 
+Apply the product of column `addr` of the operator `op` and the scalar `num` to the
+vector `v` according to the [`StochasticStyle`](@ref) of `v`. By expectation value this
+should be equivalent to
 
-```Julia
+```
 v .+= op[:, add] .* num
+```
 
 This is used to perform the spawning step in FCIQMC and to implement operator-vector
 multiplications. Mutates `v` and reports spawning statistics.
