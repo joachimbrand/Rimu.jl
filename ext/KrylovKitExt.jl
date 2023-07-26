cc6d5ac2502b7ea528bc652b8adb2ca4bb4dbf51
mtsch <matijacufar@gmail.com>
2023-07-26 23:42:12 +0200

    Fix KrylovKit extension

--- ext/KrylovKitExt.jl
+++ ext/KrylovKitExt.jl
@@ -6,16 +6,37 @@ using LinearAlgebra
 
 const U = Union{Symbol,EigSorter}
 
+"""
+    OperatorMultiplier
+
+A struct that holds the working memory for repeatedly multiplying vectors with an operator.
+"""
+struct OperatorMultiplier{H,W<:PDWorkingMemory}
+    hamiltonian::H
+    working_memory::W
+end
+function OperatorMultiplier(hamiltonian, vector::PDVec)
+    return OperatorMultiplier(hamiltonian, PDWorkingMemory(vector; style=IsDeterministic()))
+end
+
+function (o::OperatorMultiplier)(v)
+    result = scale(v, 1.0)
+    return mul!(result, o.hamiltonian, v, o.working_memory)
+end
+
 function KrylovKit.eigsolve(
     ham::AbstractHamiltonian, vec::PDVec, howmany::Int=1, which::U=:LR; kwargs...
 )
-    prop = DictVectors.OperatorMulPropagator(ham, vec)
+    # Change the type of `vec` to float, if needed.
+    v = scale!!(vec, 1.0)
+    prop = OperatorMultiplier(ham, v)
     return eigsolve(
-        prop, vec, howmany, which;
+        prop, v, howmany, which;
         ishermitian=ishermitian(ham), issymmetric=issymmetric(ham), kwargs...
     )
 end
 
+# This method only exists to detect whether a Hamiltonian is Hermitian or not.
 function KrylovKit.eigsolve(
     ham::AbstractHamiltonian, vec::AbstractDVec, howmany::Int=1, which::U=:LR; kwargs...
 )
