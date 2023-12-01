12b61c29b8914cfefcf2fbc5f01bb0f8676d4c40
Ray Yang <mingrui.yang@outlook.com>
2023-12-01 16:57:42 -0600

    Minor fix in `RimuIO.load_df` and `bose_hubbard_interaction` docstrings (#229)
    * Fix formatting in some docstrings
    
    * fix `bose_hubbard_interaction` math
    
    * Update src/BitStringAddresses/bosefs.jl
    
    Co-authored-by: mtsch <matijacufar@gmail.com>
    
    ---------
    
    Co-authored-by: Joachim Brand <j.brand@massey.ac.nz>
    Co-authored-by: mtsch <matijacufar@gmail.com>

--- src/RimuIO/RimuIO.jl
+++ src/RimuIO/RimuIO.jl
@@ -49,7 +49,7 @@ end
 
 """
     RimuIO.load_df(filename; propagate_metadata = true, add_filename = true) -> DataFrame
-Load Arrow file into `DataFrame``. Optionally propagate metadata to `DataFrame` and
+Load Arrow file into `DataFrame`. Optionally propagate metadata to `DataFrame` and
 add the file name as metadata.
 
 See also [`RimuIO.save_df`](@ref).
