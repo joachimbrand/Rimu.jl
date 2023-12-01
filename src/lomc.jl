9b28a8ec88603fa64988dd9f1838452e53057c92
Joachim Brand <joachim.brand@gmail.com>
2023-12-01 18:10:03 +1300

    fix again

--- src/lomc.jl
+++ src/lomc.jl
@@ -333,7 +333,7 @@ Alternatively, a `QMCState` can be passed in to continue a previous simulation.
 * `laststep = 100` - controls the number of steps.
 * `dτ = 0.01` - time step.
 * `targetwalkers = 1000` - target for the 1-norm of the coefficient vector.
-* `address = starting_address(ham)` - set starting address for default `v`.
+* `address = starting_address(ham)` - set starting address for default `v` and `shift`.
 * `style = IsStochasticInteger()` - set [`StochasticStyle`](@ref) for default `v`; unused
   if `v` is specified.
 * `threading` - default is to use multithreading and
@@ -395,7 +395,7 @@ julia> metadata(df2, "hamiltonian") # some metadata is automatically added
 * `maxlength = 2 * s_strat.targetwalkers + 100` - upper limit on the length of `v`; when
   reached, `lomc!` will abort
 * `params::FciqmcRunStrategy = RunTillLastStep(laststep = 100, dτ = 0.01, shift =
-  diagonal_element(ham, address(ham))` -
+  diagonal_element(ham, address)` -
   basic parameters of simulation state, see [`FciqmcRunStrategy`](@ref). Parameter values
   are overridden by explicit keyword arguments `laststep`, `dτ`, `shift`; is mutated.
 * `wm` - working memory for re-use in subsequent calculations; is mutated.
