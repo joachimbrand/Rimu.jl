b05b669b94feae6a7cffe2b69f3f46c4c2ce83a6
Rohan Kumar <rohan.06kumar@gmail.com>
2024-01-16 21:46:51 +1300

    Removed comments

--- src/Hamiltonians/correlation_functions.jl
+++ src/Hamiltonians/correlation_functions.jl
@@ -342,27 +342,6 @@ function diagonal_element(::StringCorrelator{0}, add::SingleComponentFockAddress
     return result / M
 end
 
-# function diagonal_element(::StringCorrelator{D}, add::SingleComponentFockAddress) where {D}
-#     M = num_modes(add)
-#     N = num_particles(add)
-#     n̄ = N/M
-#     d = mod(D, M)
-#     v = onr(add)
-
-#     result = 0  
-#     for i in eachindex(v)
-#         phase_sum = sum( (v[mod1(k,M)] - n̄) for k in i:1:(i+d-1) )
-
-#         result += (v[i]- n̄) * exp(pi * im * phase_sum) * (v[mod1(i + d, M)]-n̄)
-#     end
-
-#     if M == N
-#         return real(result) /M
-#     end
-
-#     return result / M
-# end
-
 num_offdiagonals(::StringCorrelator, ::SingleComponentFockAddress) = 0
 
 function diagonal_element(::StringCorrelator{D}, add::SingleComponentFockAddress) where {D}
