348892c86485383ce2f5dd337f3f468852268a13
mtsch <matijacufar@gmail.com>
2024-02-15 18:35:50 +1300

    Example script polishing (#239)
    * Reword, add punctuation, etc.
    
    * Fix doc links
    
    * Minor fixes, better plot
    
    * Update size_threshold for large plot
    
    * tweak plot
    
    * use IsDynamicSemistochastic(), more tweaks
    
    * Apply suggestions from code review
    
    Co-authored-by: Joachim Brand <j.brand@massey.ac.nz>
    
    * Attempt to fix labels
    
    * Apply suggestions from code review
    
    Co-authored-by: Joachim Brand <j.brand@massey.ac.nz>
    
    * can't have both branches: and branches-ignore: at the same time
    
    * modify margin
    
    ---------
    
    Co-authored-by: Joachim Brand <j.brand@massey.ac.nz>

--- scripts/BHM-example.jl
+++ scripts/BHM-example.jl
@@ -1,121 +1,154 @@
 # # Example 1: 1D Bose-Hubbard Model
 
-# This is an example calculation finding the ground state of
-# a 1D Bose-Hubbard chain with 6 particles in 6 lattice sites.
+# This is an example calculation finding the ground state of a 1D Bose-Hubbard chain with 6
+# particles in 6 lattice sites.
 
 # A runnable script for this example is located
 # [here](https://github.com/joachimbrand/Rimu.jl/blob/develop/scripts/BHM-example.jl).
 # Run it with `julia BHM-example.jl`.
 
-# Firstly, we load all needed modules.
-# `Rimu` for FCIQMC calculation;
+# First, we load Rimu and Plots.
 
 using Rimu
-using Plots # for plotting
+using Plots
 
 # ## Setting up the model
 
-# Now we define the physical problem:
-# Generating a configuration where 6 particles are evenly distributed in 6 lattice sites:
-aIni = near_uniform(BoseFS{6,6})
-# where `BoseFS` is used to create a bosonic Fock state.
-# The Hamiltonian is defined by specifying the model and the parameters.
-# Here we use the Bose Hubbard model in one dimension and real space:
-Ĥ = HubbardReal1D(aIni; u = 6.0, t = 1.0)
+# We start by defining the physical problem. First, we generate an initial configuration
+# which will be used as a starting point of our computation. In this example, we use a
+# bosonic Fock state with 6 particles evenly distributed in 6 lattice sites.
+initial_address = near_uniform(BoseFS{6,6})
+
+# The Hamiltonian is constructed by initializing a struct with an initial address and model
+# parameters. Here, we use the Bose Hubbard model in one-dimensional real space.
+H = HubbardReal1D(initial_address; u = 6.0, t = 1.0)
 
 # ## Parameters of the calculation
 
-# Now let's setup the Monte Carlo calculation.
-# We need to decide the number of walkers to use in this Monte Carlo run, which is
-# equivalent to the average one-norm of the coefficient vector:
+# Now, let's setup the Monte Carlo calculation. We need to decide the number of walkers to
+# use in this Monte Carlo run, which is equivalent to the average one-norm of the
+# coefficient vector. Higher values will result in better statistics, but require more
+# memory and computing power.
 targetwalkers = 1_000;
 
-# It is good practice to equilibrate the time series before taking statistics.
+# FCIQMC takes a certain number of steps to equllibrate, after which the observables will
+# fluctuate around a mean value. In this example, we will devote 1000 steps to equilibration
+# and take an additional 2000 steps for measurement.
 steps_equilibrate = 1_000;
 steps_measure = 2_000;
+laststep = steps_equilibrate + steps_measure
 
-# The appropriate size of the time step is problem dependent.
+# Next, we pick a time step size. FCIQMC does not have a time step error, but the
+# time step needs to be small enough, or the computation might diverge. If the time step is
+# too small, however, the computation might take a long time to equilibrate. The
+# appropriate time step size is problem-dependent and is best determined through
+# experimentation.
 dτ = 0.001;
 
 # ## Defining an observable
 
-# Now let's set up an observable to measure. Here we will measure the projected energy.
-# In additon to the `shift`, the projected energy is a second estimator for the energy.
+# Now, let's set up an observable to measure. Here we will measure the projected energy. In
+# additon to the shift, the projected energy is a second estimator for the energy. It
+# usually produces better statistics than the shift.
 
-# We first need to define a projector. Here we use the function `default_starting_vector`
-# to generate a vector with only a single occupied configuration, the same vector that we
-# will use as starting vector for the FCIQMC calculation.
-svec = default_starting_vector(aIni; style=IsStochasticInteger())
-# The choice of the `style` argument already determines the FCIQMC algorithm to use
-# (while it is irrelevant for the projected energy).
+# We first need to define a projector. Here, we use the function
+# [`default_starting_vector`](@ref) to generate a vector with only a single occupied
+# configuration. We will use the same vector as the starting vector for the FCIQMC
+# calculation.
+initial_vector = default_starting_vector(initial_address; style=IsDynamicSemistochastic())
 
-# Observables are passed into the `lomc!` function with the `post_step` keyword argument.
-post_step = ProjectedEnergy(Ĥ, svec)
+# The choice of the `style` argument already determines the FCIQMC algorithm to
+# use. [`IsDynamicSemistochastic`](@ref) is usually the best choice as it reduces noise and
+# improves the sign problem.
+
+# Observables that can be calculated by projection of the fluctuating quantum state onto
+# a constant vector are passed into the [`lomc!`](@ref) function with the `post_step`
+# keyword argument.
+post_step = ProjectedEnergy(H, initial_vector)
 
 # ## Running the calculation
 
-# Seeding the random number generator is sometimes useful in order to get reproducible
-# results
+# In this example, we seed the random number generator in order to get reproducible results.
+# This should not be done for actual computations.
 using Random
 Random.seed!(17);
 
-# Finally, we can start the main FCIQMC loop:
-df, state = lomc!(Ĥ, svec;
-            laststep = steps_equilibrate + steps_measure, # total number of steps
-            dτ,
-            targetwalkers,
-            post_step,
+# Finally, we can start the FCIQMC run.
+df, state = lomc!(
+    H, initial_vector;
+    laststep,
+    dτ,
+    targetwalkers,
+    post_step,
 );
 
-# `df` is a `DataFrame` containing the time series data.
+# Here, `df` is a `DataFrame` containing the time series data, while `state` contains the
+# internal state of FCIQMC, which can be used to continue computations.
 
 # ## Analysing the results
 
-# We can plot the norm of the coefficient vector as a function of the number of steps:
-hline([targetwalkers], label="targetwalkers", color=:red, linestyle=:dash)
-plot!(df.steps, df.norm, label="norm", ylabel="norm", xlabel="steps")
-# After some equilibriation steps, the norm fluctuates around the target number of walkers.
+# We can plot the norm of the coefficient vector as a function of the number of steps.
+hline(
+    [targetwalkers];
+    label="targetwalkers", xlabel="steps", ylabel="norm",
+    color=2, linestyle=:dash, margin = 1Plots.cm
+)
+plot!(df.steps, df.norm, label="norm", color=1)
 
-# Now let's look at estimating the energy from the shift.
-# The mean of the shift is a useful estimator of the shift. Calculating the error bars
-# is a bit more involved as correlations have to be removed from the time series.
-# The following code does that with blocking transformations:
+# After an initial equilibriation period, the norm fluctuates around the target number of
+# walkers.
+
+# Now, let's look at using the shift to estimate the ground state energy of `H`. The mean of
+# the shift is a useful estimator of the energy. Calculating the error bars is a bit more
+# involved as autocorrelations have to be removed from the time series. This can be done
+# with the function [`shift_estimator`](@ref), which performs a blocking analysis.
 se = shift_estimator(df; skip=steps_equilibrate)
 
-# For the projected energy, it a bit more complicated as it's a ratio of fluctuationg
-# quantities:
+# Here, `se` contains the calculated mean and standard errors of the shift, as well as some
+# additional information related to the blocking analysis.
+
+# Computing the error of the projected energy is a bit more complicated, as it's a ratio of
+# fluctuating variables. Thankfully, the complications are handled by the following
+# function.
 pe = projected_energy(df; skip=steps_equilibrate)
 
-# The result is a ratio distribution. Let's get its median and lower and upper error bars
-# for a 95% confidence interval
+# The result is a ratio distribution. We extract its median and the edges of the 95%
+# confidence interval.
 v = val_and_errs(pe; p=0.95)
 
-# Let's visualise these estimators together with the time series of the shift
-plot(df.steps, df.shift, ylabel="energy", xlabel="steps", label="shift")
+# Let's visualise these estimators together with the time series of the shift.
+plot(df.steps, df.shift, ylabel="energy", xlabel="steps", label="shift", margin = 1Plots.cm)
 
 plot!(x->se.mean, df.steps[steps_equilibrate+1:end], ribbon=se.err, label="shift mean")
 plot!(
     x -> v.val, df.steps[steps_equilibrate+1:end], ribbon=(v.val_l,v.val_u),
-    label="projected_energy",
+    label="projected energy",
 )
-# In this case the projected energy and the shift are close to each other an the error bars
-# are hard to see on this scale.
+lens!([steps_equilibrate, laststep], [-5.1, -2.9]; inset=(1, bbox(0.2, 0.25, 0.6, 0.4)))
+
+# In this case the projected energy and the shift are close to each other and the error bars
+# are hard to see.
 
 # The problem was just a toy example, as the dimension of the Hamiltonian is rather small:
-dimension(Ĥ)
+dimension(H)
 
-# In this case is easy (and more efficient) to calculate the exact ground state energy
-# using standard linear algebra:
+# In this case, it's easy (and more efficient) to calculate the exact ground state energy
+# using standard linear algebra. Read more about Rimu's capabilities for exact
+# diagonalisation in the example "Exact diagonalisation".
+
 using LinearAlgebra
-exact_energy = eigvals(Matrix(Ĥ))[1]
-# Read more about `Rimu.jl`s capabilities for exact diagonalisation in the example
-# "Exact diagonalisation".
+exact_energy = eigvals(Matrix(H))[1]
 
-# Comparing our results for the energy:
-println("Energy from $steps_measure steps with $targetwalkers walkers:
-         Shift: $(se.mean) ± $(se.err)
-         Projected Energy: $(v.val) ± ($(v.val_l), $(v.val_u))
-         Exact Energy: $exact_energy")
+# We finish by comparing our FCIQMC results with the exact computation.
+
+println(
+    """
+    Energy from $steps_measure steps with $targetwalkers walkers:
+    Shift: $(se.mean) ± $(se.err)
+    Projected Energy: $(v.val) ± ($(v.val_l), $(v.val_u))
+    Exact Energy: $exact_energy
+    """
+)
 
 
 using Test                                      #hide
--- scripts/HO-example.jl
+++ scripts/HO-example.jl
@@ -1,19 +1,18 @@
 # # Example 5: Degenerate perturbation theory in a harmonic oscillator basis
 
-
-# `Rimu` can also handle non-lattice systems. This example looks at 
-# weakly-interacting bosonic particles in a harmonic oscillator external 
-# potential using a basis of (Cartesian product) single-particle 
-# eigenstates of the harmonic oscillator potential. 
-# Blocks of degenerate non-interacting states are coupled by a 
-# contact interaction in first order degenerate perturbation theory. This example shows 
+# `Rimu` can also handle non-lattice systems. This example looks at
+# weakly-interacting bosonic particles in a harmonic oscillator external
+# potential using a basis of (Cartesian product) single-particle
+# eigenstates of the harmonic oscillator potential.
+# Blocks of degenerate non-interacting states are coupled by a
+# contact interaction in first order degenerate perturbation theory. This example shows
 # how to generate these blocks and find the energy and angular momentum eigenstates.
 
 # A runnable script for this example is located
 # [here](https://github.com/joachimbrand/Rimu.jl/blob/develop/scripts/HO-example.jl).
 # Run it with `julia HO-example.jl`.
 
-# First, load all needed modules.
+# First, load all needed packages.
 using Rimu
 using DataFrames
 using LinearAlgebra
@@ -21,100 +20,105 @@ using LinearAlgebra
 # Define the system size for ``N=2`` particles in a 2D harmonic oscillator
 # allowing ``M=4`` levels in each dimension, including the groundstate.
 N = 2
-M = 4
+M = 4;
 
 # Use a tuple `S` to define the range of harmonic oscillator states in a Cartesian
 # basis, in this isotropic case ``n_x,n_y=0,1,\ldots,M-1``.
-S = (M, M)
+S = (M, M);
 
-# In `Rimu` the ``N``-particle states are still stored as Fock states
+# In `Rimu` the ``N``-particle states are still stored as Fock states.
 P = prod(S)
 addr = BoseFS(P, M => N)
-# where the numbering of the modes folds in the two spatial dimensions. 
-# Use the utility function [`fock_to_cart`](@ref) to convert a Fock address to 
-# human-readable Cartesian quantum numbers for inspection.
+
+# Here, the numbering of the modes folds in the two spatial dimensions.  Use the utility
+# function [`fock_to_cart`](@ref) to convert a Fock address to human-readable Cartesian
+# quantum numbers for inspection.
 fock_to_cart(addr, S)
+
 # The output shows that all ``N`` particles are in single-particle state ``n_x=M-1, n_y=0``.
 
-# The harmonic oscillator Hamiltonian [`HOCartesianContactInteractions`](@ref) handles 
+# The harmonic oscillator Hamiltonian [`HOCartesianContactInteractions`](@ref) handles
 # contact interactions with
 # first-order perturbation theory, so the matrix representation will block
-# according to the non-interacting energy of the basis states. The first task is 
-# to find all blocks of basis states with the same energy. The strength of the 
+# according to the non-interacting energy of the basis states. The first task is
+# to find all blocks of basis states with the same energy. The strength of the
 # interaction is not relevant at this point, just that it is non-zero.
-# Use a dummy groundstate address to build the Hamiltonian 
-H = HOCartesianContactInteractions(BoseFS(P, 1 => N); S)
-# and then a utility function [`get_all_blocks`](@ref) to find all blocks. The blocks 
-# are found by looping over all possible states with `N` particles in Cartesian states 
-# defined by `S`. Note that this will only work for total energy 
+# Use an arbitrary `N`-particle starting address to build the Hamiltonian.
+
+H = HOCartesianContactInteractions(BoseFS(P, 1 => N); S);
+
+# Then, use the utility function [`get_all_blocks`](@ref) to find all blocks. The blocks
+# are found by looping over all possible states with `N` particles in Cartesian states
+# defined by `S`. Note that this will only work for total energy
 # up to the maximum accessible by a single particle.
-# The ``N``-particle groundstate energy for a 2D harmonic oscillator is 
+# The ``N``-particle groundstate energy for a 2D harmonic oscillator is
 # ``E_0 = N \hbar \omega`` and the maximum single-particle energy is ``E = (E_0 + M - 1) \hbar \omega``.
 block_df = get_all_blocks(H; max_energy = N + M - 1)
 
 # This outputs a list of blocks in `H` indexed by the noninteracting energy
-# of all states in the block, and a single address that can be used to 
+# of all states in the block, and a single address that can be used to
 # rebuild the block for further analysis.
 addr1 = block_df[7,:addr]
 E = block_df[7,:block_E0]
-# First, notice that all basis states have the same energy, defined by the block
+
+# First, notice that all basis states have the same energy, defined by the block.
 basis1 = build_basis(H, addr1)
 map(b -> Hamiltonians.noninteracting_energy(H, b), basis1)
 
-# There are two blocks at each energy level (except the groundstate), which 
-# are different due to parity conservation, which is the only other symmetry in the 
+# There are two blocks at each energy level (except the groundstate), which
+# are different due to parity conservation, which is the only other symmetry in the
 # Cartesian harmonic oscillator.
-# The basis of this other block is different 
+# The basis of this other block is different,
 addr2 = block_df[4,:addr]
 basis2 = build_basis(H, addr2);
 basis1 ≠ basis2
-# but its basis elements have the same energy
+
+# but its basis elements have the same energy.
 map(b -> Hamiltonians.noninteracting_energy(H, b), basis2)
 
-# However, since this system is an isotropic harmonic oscillator, it is possible 
+# However, since this system is an isotropic harmonic oscillator, it is possible
 # to build simultaneous eigenstates of the angular momentum operator
-# ``L_z``, implemented with [`AxialAngularMomentumHO`](@ref)
+# ``L_z``, implemented with [`AxialAngularMomentumHO`](@ref).
 Lz = AxialAngularMomentumHO(S)
 
-# ``L_z`` does not conserve parity so both blocks are required. First combine the bases 
-# of each block and convert to `DVecs`
+# ``L_z`` does not conserve parity, so both blocks are required. First combine the bases
+# of each block and convert to [`DVec`](@ref)s.
 dvs = map(b -> DVec(b => 1.0), vcat(basis1, basis2));
-# and then compute overlaps for the matrix elements of ``L_z``
+
+# and then compute overlaps for the matrix elements of ``L_z``.
 Lz_mat = [dot(v, Lz, w) for v in dvs, w in dvs]
 
 # By diagonalising this matrix the eigenstate have energy `E` and well-defined angular
-# momentum
+# momentum.
 
-# Diagonalise this matrix to obtain the eigenstates of ``L_z``. 
-# The eigenvectors provide the linear combinations of basis states with well-defined 
+# Diagonalise this matrix to obtain the eigenstates of ``L_z``.
+# The eigenvectors provide the linear combinations of basis states with well-defined
 # angular momentum, within the subspace of energy ``E``.
 Lz_vals, Lz_vecs = eigen(Lz_mat)
 
-# Finally, consider the effect of interactions by looking at how states 
-# in a single block are perturbed. Only the energy shift due to the interaction is 
-# relevant so now rebuild the Hamiltonian without the non-interacting energy
+# Finally, consider the effect of interactions by looking at how states
+# in a single block are perturbed. Only the energy shift due to the interaction is
+# relevant so now rebuild the Hamiltonian without the non-interacting energy.
 Hint = HOCartesianContactInteractions(addr1; S, interaction_only = true)
 ΔE = eigvals(Matrix(Hint, addr1))
 
-# Two eigenstates in this block are unaffected by the interaction and three have a 
+# Two eigenstates in this block are unaffected by the interaction and three have a
 # non-zero energy shift.
 
 # The default strength of the interaction is `g = 1.0`. Other interactions strengths
 # can be obtained by using keyword argument `g` in `HOCartesianContactInteractions` or by
 # rescaling `ΔE` since the interactions are handled with first-order perturbation theory.
 
-# `Rimu` also contains [`HOCartesianEnergyConservedPerDim`](@ref) which is a similar 
+# Rimu also contains [`HOCartesianEnergyConservedPerDim`](@ref) which is a similar
 # Hamiltonian but with the stricter condition that the contact interaction only connects
-# states that have the same total energy in each dimension, rather than conserving the 
+# states that have the same total energy in each dimension, rather than conserving the
 # overall total energy. Both Hamiltonians can handle anisotropic systems by passing a tuple
 # `S` whose elements are not all the same. This will alter which states are connected by the
-# interaction, but assumes that the harmonic trapping frequencies in each dimension are 
-# commensurate
-
-# Finished!
+# interaction, but assumes that the harmonic trapping frequencies in each dimension are
+# commensurate.
 
 using Test                                      #hide
 @test nrow(block_df) == 2*(M-1) + 1             #hide
-@test E == N + M - 1                           #hide
+@test E == N + M - 1                            #hide
 @test Lz_vals ≈ [-3,-3,-1,-1,-1,1,1,1,3,3]      #hide
 nothing                                         #hide
