22709a28382e82431a5ff03824a5a11d2e6ed685
Rohan Kumar <rohan.06kumar@gmail.com>
2024-02-22 01:41:41 +1300

    Edited FroehlichPolaron Constructor so that it checks whether the starting address abides by mode/momentum cutoffs. If not, ArgumentErrors are thrown.

--- src/Hamiltonians/FroehlichPolaron.jl
+++ src/Hamiltonians/FroehlichPolaron.jl
@@ -1,30 +1,37 @@
 """
-    FroehlichPolaron(
-        address;
-        alpha = 1.0,
-        total_mom = 0.0,
-        geometry = nothing,
-        num_dimensions = 1
-    )
+function FroehlichPolaron(
+    addr::OccupationNumberFS;
+    v=1.0,
+    mass=1.0,
+    omega=1.0,
+    l=1.0,
+    p=0.0,
+    momentum_cutoff=nothing,
+    mode_cutoff=100.0,
+)
 
-The Froehlich polaron Hamiltonian is given by
+The 1D Froehlich polaron Hamiltonian is given by
 
 ```math
 H = \\frac{(P̂_f - P)^2}{m} + ωN̂ - ν Σₖ(âₖ^† + â_{-k})
 ```
 
-where ``P`` is the total momentum `total_mom`, ``P̂_f = \\frac{M}{L}Σ_k k âₖ^† âₖ`` is the momentum
-operator for the bosons, ``N̂ = Σ_k âₖ^† âₖ`` is the number operator for the bosons, and
-``νₖ = \\sqrt{α}/|k|`` is the coupling strength determined by the coupling parameter
-`α == alpha`.
-
-The optional `geometry` argument specifies the geometry of the lattice for ``k``-space and
-should be of the type [`PeriodicBoundaries`](@ref). A simplified way of specifying the
-geometry is to provide the number of dimensions `num_dimensions`. In this case the
-[`num_modes(address)`](@ref) of `address` must be a square number for `num_dimensions = 2`,
-or a cube number for `num_dimensions = 3`.
+where ``P`` is the total momentum `total_mom`, ``P̂_f = \\frac{M}{L}Σ_k k âₖ^† âₖ`` is the momentum operator for the bosons, ``N̂ = Σ_k âₖ^† âₖ`` is the number operator for the bosons, and ``ν`` is the coupling strength. Here, ``M`` is the number of discrete momentum modes and ```L`` is the momentum scaling.
 
 The `address` must be of type [`OccupationNumberFS`](@ref).
+
+# Arguments
+
+* `addr`: the starting address.
+* `v`: the coupling strength
+* `mass`: the particle mass
+* `omega`: the oscillation frequency of the phonons
+* `l`: the next-neighbor interaction
+* `p`: the total momentum
+* `momentum_cutoff`: the maximum total momentum allowed for a basis element
+* `mode_cutoff`: the maximum population in each momentum mode
+
+
 """
 struct FroehlichPolaron{
     T, # eltype
@@ -51,8 +58,12 @@ function FroehlichPolaron(
     l=1.0,
     p=0.0,
     momentum_cutoff=nothing,
-    mode_cutoff=100.0,
+    mode_cutoff=10.0,
 )
+    if _exceed_mode_cutoff(mode_cutoff,addr)
+        throw(ArgumentError("Starting address cannot have occupations that exceed mode_cutoff"))
+    end
+
     M = num_modes(addr) # this is compile-time information
     if isnothing(momentum_cutoff)
         v, p, mass, omega, l, mode_cutoff = promote(float(v), float(p), float(mass), float(omega), float(l), float(mode_cutoff))
@@ -68,6 +79,14 @@ function FroehlichPolaron(
     end
     kr = range(start; step = step, length = M)
     ks = SVector{M}(kr)
+
+    if !isnothing(momentum_cutoff)
+        momentum = (M/l) * dot(ks,onr(addr))
+        if momentum > momentum_cutoff
+            throw(ArgumentError("Starting address has momentum $momentum which cannot exceed momentum_cutoff $momentum_cutoff"))
+        end
+    end
+
     return FroehlichPolaron(addr, v, mass, omega, l, p, ks, momentum_cutoff, mode_cutoff)
 end
 
@@ -82,19 +101,15 @@ end
 
 LOStructure(::Type{<:FroehlichPolaron{<:Real}}) = IsHermitian()
 
-# Base.getproperty(h::FroehlichPolaron, s::Symbol) = getproperty(h, Val(s))
-# Base.getproperty(h::FroehlichPolaron, ::Val{:ks}) = getfield(h, :ks)
-# Base.getproperty(h::FroehlichPolaron, ::Val{:addr}) = getfield(h, :addr)
-# Base.getproperty(h::FroehlichPolaron, ::Val{:v}) = getfield(h, :v)
-# Base.getproperty(h::FroehlichPolaron, ::Val{:mass}) = getfield(h, :mass)
-# Base.getproperty(h::FroehlichPolaron, ::Val{:omega}) = getfield(h, :omega)
-# Base.getproperty(h::FroehlichPolaron, ::Val{:l}) = getfield(h, :l)
-# Base.getproperty(h::FroehlichPolaron, ::Val{:p}) = getfield(h, :p)
+# The optional `geometry` argument specifies the geometry of the lattice for ``k``-space and
+# should be of the type [`PeriodicBoundaries`](@ref). A simplified way of specifying the
+# geometry is to provide the number of dimensions `num_dimensions`. In this case the
+# [`num_modes(address)`](@ref) of `address` must be a square number for `num_dimensions = 2`,
+# or a cube number for `num_dimensions = 3`.
 
 ks(h::FroehlichPolaron) = getfield(h, :ks)
 
 # TODO: Sort out scales for momentum and energy; additional parameters?
-# TODO: compute diagonal and off-diagonal elements
 # TODO: rest of AbstractHamiltonian interface
 # TODO: write unit tests for FroehlichPolaron
 
@@ -117,7 +132,7 @@ function get_offdiagonal(h::FroehlichPolaron{T,M,<:Any,T}, addr::OccupationNumbe
     naddress, value = _froehlich_offdiag(h,addr,chosen)
     
     new_p_tot = dot(h.ks, onr(naddress))
-    if new_p_tot > h.momentum_cutoff # check if momentum of new address exceeds momentum_cutoff
+    if (M/h.l) * new_p_tot > h.momentum_cutoff # check if momentum of new address exceeds momentum_cutoff
         return addr, 0.0
     else
         return naddress, - h.v * value
@@ -137,4 +152,8 @@ function _froehlich_offdiag(h, addr::OccupationNumberFS{M},chosen) where {M}
         naddress, value = excitation(addr, (),(chosen-M,))
         return naddress, - h.v * value
     end
+end
+
+function _exceed_mode_cutoff(mode_cutoff, addr::OccupationNumberFS{M}) where {M}
+    return any(x->x>mode_cutoff,onr(addr))
 end
