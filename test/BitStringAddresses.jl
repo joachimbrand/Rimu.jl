aabceea04d61ecba4e55555920be12ebff3c8e8e
Skuwar1 <168614969+Skuwar1@users.noreply.github.com>
2024-06-10 16:03:00 +1200

    Update BitStringAddresses.jl

--- test/BitStringAddresses.jl
+++ test/BitStringAddresses.jl
@@ -577,10 +577,10 @@ end
     ffs1 = FermiFS{1,4}(1,0,0,0)
     ffsend = FermiFS{1,4}(0,0,0,1)
 
-    @test hopnextneighbour(ffs1,2; pitwisted=true) == (ffsend, -1.0)
-    @test hopnextneighbour(ffsend,1; pitwisted=true) == (ffs1, -1.0)
-    @test hopnextneighbour(ffs1,2; hardwall=true) == (ffsend, 0.0)
-    @test hopnextneighbour(ffsend,1; hardwall=true) == (ffs1, 0.0)
+    @test hopnextneighbour(ffs1,2, Val(true), Val(false)) == (ffsend, -1.0)
+    @test hopnextneighbour(ffsend,1, Val(true), Val(false)) == (ffs1, -1.0)
+    @test hopnextneighbour(ffs1,2, Val(false), Val(true)) == (ffsend, 0.0)
+    @test hopnextneighbour(ffsend,1, Val(false), Val(true)) == (ffs1, 0.0)
     @test hopnextneighbour(ffs1,2) == (ffsend, 1.0)
     @test hopnextneighbour(ffsend,1) == (ffs1, 1.0)
 
