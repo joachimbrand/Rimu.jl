8a420629ddef343e331a091aae34a75d7c2edac3
Skuwar1 <168614969+Skuwar1@users.noreply.github.com>
2024-06-11 16:47:09 +1200

    Update Hamiltonians.jl

--- test/Hamiltonians.jl
+++ test/Hamiltonians.jl
@@ -1687,4 +1687,9 @@ end
         end
         @test get_offdiagonal(H, addr, 2)[1] == addr2
     end
+    H1 = ExtendedHubbardReal1D(BoseFS(1,1,1,1))
+    H2 = ExtendedHubbardReal1D(BoseFS(1,1,1,1); boundary_condition=:hard_wall)
+    H3 = ExtendedHubbardReal1D(BoseFS(1,1,1,1); boundary_condition=:twisted)
+    @test sparse(H1) == sparse(H2)
+    @test sparce(H1) == sparse(H3)
 end
