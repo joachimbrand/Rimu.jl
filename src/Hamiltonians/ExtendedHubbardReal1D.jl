41d35ec536c717434a3ebd20f45658f154161245
Joachim Brand <joachim.brand@gmail.com>
2024-07-29 19:10:51 +1200

    prettify

--- src/Hamiltonians/ExtendedHubbardReal1D.jl
+++ src/Hamiltonians/ExtendedHubbardReal1D.jl
@@ -1,10 +1,12 @@
 """
     ExtendedHubbardReal1D(address; u=1.0, v=1.0, t=1.0, boundary_condition=:periodic)
 
-Implements the extended Hubbard model on a one-dimensional chain in real space. This Hamiltonian can be either real or complex, depending on the choice of `boundary_condition`.
+Implements the extended Hubbard model on a one-dimensional chain in real space. This
+Hamiltonian can be either real or complex, depending on the choice of `boundary_condition`.
 
 ```math
-\\hat{H} = -t \\sum_{\\langle i,j\\rangle} a_i^† a_j + \\frac{u}{2}\\sum_i n_i (n_i-1) + v \\sum_{\\langle i,j\\rangle} n_i n_j
+\\hat{H} = -t \\sum_{\\langle i,j\\rangle} a_i^† a_j + \\frac{u}{2}\\sum_i n_i (n_i-1) +
+v \\sum_{\\langle i,j\\rangle} n_i n_j
 ```
 
 # Arguments
@@ -16,12 +18,14 @@ Implements the extended Hubbard model on a one-dimensional chain in real space. 
 * `boundary_condition` The following values are supported:
   * `:periodic`: usual period boundary condition realising a ring geometry.
   * `:hard_wall`: hopping over the boundary is not allowed.
-  * `:twisted`: like `:periodic` but hopping over the boundary incurs an additional factor of `-1`.
-  * `θ <: Number`: like `:periodic` and `:twisted` but hopping over the boundary incurs a factor
-  ``\\exp(iθ)`` for a hop to the right and ``\\exp(−iθ)`` for a hop to the left. With this choice the
-  Hamiltonian will have a complex `eltype` whereas otherwise the `eltype` is deterimed by the
-  type of the parameters `t`, `u`, and `v`.
+  * `:twisted`: like `:periodic` but hopping over the boundary incurs an additional factor
+    of `-1`.
+  * `θ <: Number`: like `:periodic` and `:twisted` but hopping over the boundary incurs a
+    factor``\\exp(iθ)`` for a hop to the right and ``\\exp(−iθ)`` for a hop to the left.
+    With this choice the Hamiltonian will have a complex `eltype` whereas otherwise the
+    `eltype` is deterimed by the type of the parameters `t`, `u`, and `v`.
 
+See also [`HubbardRealSpace`](@ref).
 """
 struct ExtendedHubbardReal1D{TT,A<:SingleComponentFockAddress,U,V,T,BOUNDARY_CONDITION} <: AbstractHamiltonian{TT}
     address::A
@@ -63,10 +67,15 @@ end
 
 Base.getproperty(h::ExtendedHubbardReal1D, s::Symbol) = getproperty(h, Val(s))
 Base.getproperty(h::ExtendedHubbardReal1D, ::Val{:address}) = getfield(h, :address)
-Base.getproperty(h::ExtendedHubbardReal1D{<:Any,<:Any,U}, ::Val{:u}) where U = U
-Base.getproperty(h::ExtendedHubbardReal1D{<:Any,<:Any,<:Any,V}, ::Val{:v}) where V = V
-Base.getproperty(h::ExtendedHubbardReal1D{<:Any,<:Any,<:Any,<:Any,T}, ::Val{:t}) where T = T
-Base.getproperty(h::ExtendedHubbardReal1D{<:Any,<:Any,<:Any,<:Any,<:Any,BOUNDARY_CONDITION}, ::Val{:boundary_condition}) where BOUNDARY_CONDITION = BOUNDARY_CONDITION
+Base.getproperty(::ExtendedHubbardReal1D{<:Any,<:Any,U}, ::Val{:u}) where U = U
+Base.getproperty(::ExtendedHubbardReal1D{<:Any,<:Any,<:Any,V}, ::Val{:v}) where V = V
+Base.getproperty(::ExtendedHubbardReal1D{<:Any,<:Any,<:Any,<:Any,T}, ::Val{:t}) where T = T
+function Base.getproperty(
+    ::ExtendedHubbardReal1D{<:Any,<:Any,<:Any,<:Any,<:Any,BOUNDARY_CONDITION},
+    ::Val{:boundary_condition}
+) where BOUNDARY_CONDITION
+    BOUNDARY_CONDITION
+end
 
 function num_offdiagonals(::ExtendedHubbardReal1D, address::SingleComponentFockAddress)
     return 2 * num_occupied_modes(address)
