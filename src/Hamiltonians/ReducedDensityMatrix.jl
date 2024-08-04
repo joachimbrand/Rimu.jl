1e8031c98da14d9823d42accf4e08b72b2737f44
Skuwar1 <168614969+Skuwar1@users.noreply.github.com>
2024-08-05 11:22:33 +1200

    Update ReducedDensityMatrix.jl
    - Changed names of the operators
    - debugged the code for off-diagonal terms in operators.

--- src/Hamiltonians/ReducedDensityMatrix.jl
+++ src/Hamiltonians/ReducedDensityMatrix.jl
@@ -1,32 +1,32 @@
 """
-    SingleParticleReducedDensityMatrix(i, j) <: AbstractHamiltonian
+    SingleParticleExcitation(i, j) <: AbstractHamiltonian
 
-Represent a {i,j} element of the single-particle reduced density matrix:
+Represent {i,j} element of the single-particle reduced density matrix:
 
 ```math
-\\hat{ρ}^{(1)}_{i,j} = \\langle \\psi | \\hat a^†_{i} \\hat a_{j} | \\psi \\rangle
+\\hat{ρ}^{(1)}_{i,j} = \\hat a^†_{i} \\hat a_{j}
 ```
 
-where ``i`` and ``j`` are the `mode` and ``| \\psi \\rangle`` is the `state-ket` of the given Hamiltonian.
+where ``i`` and ``j`` are the `mode`
 
 # See also
 
 * [`single_particle_density`](@ref)
 * [`SingleParticleDensity`](@ref)
-* [`TwoParticleReducedDensityMatrix`](@ref)
+* [`TwoParticleExcitation`](@ref)
 """
-struct SingleParticleReducedDensityMatrix <: AbstractHamiltonian{Float64}
+struct SingleParticleExcitation <: AbstractHamiltonian{Float64}
     I::Int
     J::Int
 end
 
-function Base.show(io::IO, spd::SingleParticleReducedDensityMatrix)
-    print(io, "SingleParticleReducedDensityMatrix($(spd.I), $(spd.J))")
+function Base.show(io::IO, spd::SingleParticleExcitation)
+    print(io, "SingleParticleExcitation($(spd.I), $(spd.J))")
 end
 
-LOStructure(::Type{T}) where T<:SingleParticleReducedDensityMatrix = AdjointUnknown()
+LOStructure(::Type{T}) where T<:SingleParticleExcitation = AdjointUnknown()
 
-function diagonal_element(spd::SingleParticleReducedDensityMatrix, add::SingleComponentFockAddress)
+function diagonal_element(spd::SingleParticleExcitation, add::SingleComponentFockAddress)
     src = find_mode(add, spd.J)
     dst = find_mode(add,spd.I)
     address, value = excitation(add, (dst,), (src,))
@@ -37,59 +37,52 @@ function diagonal_element(spd::SingleParticleReducedDensityMatrix, add::SingleCo
     end
 end
 
-function num_offdiagonals(spd::SingleParticleReducedDensityMatrix, address::SingleComponentFockAddress)
+function num_offdiagonals(spd::SingleParticleExcitation, address::SingleComponentFockAddress)
     if spd.I == spd.J
         return 0
     else
-        return 2
+        return 1
     end
 end
 
-function get_offdiagonal(spd::SingleParticleReducedDensityMatrix, add::SingleComponentFockAddress, chosen)
+function get_offdiagonal(spd::SingleParticleExcitation, add::SingleComponentFockAddress, chosen)
     src = find_mode(add, spd.J)
     dst = find_mode(add,spd.I)
-    if chosen == 2
-        src, dst = dst, src
-    end
     address, value = excitation(add, (dst,), (src,))
-    if (chosen == 2 && add isa FermiFS)
-        return address, -value/2
-    else
-        return address, value/2
-    end
+    return address, value
 end
 
 """
-    TwoParticleReducedDensityMatrix(i, j, k, l) <: AbstractHamiltonian
+    TwoParticleExcitation(i, j, k, l) <: AbstractHamiltonian
 
-Represent a {ij, kl} element of the two-particle reduced density matrix:
+Represent {ij, kl} element of the two-particle reduced density matrix:
 
 ```math
-\\hat{ρ}^{(2)}_{ij, kl} = \\langle \\psi | \\hat a^†_{i} \\hat a^†_{j} \\hat a_{l} \\hat a_{k} | \\psi \\rangle
+\\hat{ρ}^{(2)}_{ij, kl} =  \\hat a^†_{i} \\hat a^†_{j} \\hat a_{l} \\hat a_{k} 
 ```
 
-where ``i``, ``j``, ``k``, and ``l`` are the `mode` and ``| \\psi \\rangle`` is the `state-ket` of the given Hamiltonian.
+where ``i``, ``j``, ``k``, and ``l`` are the `mode`
 
 # See also
 
 * [`single_particle_density`](@ref)
 * [`SingleParticleDensity`](@ref)
-* [`SingleParticleReducedDensityMatrix`](@ref)
+* [`SingleParticleExcitation`](@ref)
 """
-struct TwoParticleReducedDensityMatrix <: AbstractHamiltonian{Float64}
+struct TwoParticleExcitation <: AbstractHamiltonian{Float64}
     I::Int
     J::Int
     K::Int
     L::Int
 end
 
-function Base.show(io::IO, spd::TwoParticleReducedDensityMatrix)
-    print(io, "TwoParticleReducedDensityMatrix($(spd.I), $(spd.J), $(spd.K), $(spd.L))")
+function Base.show(io::IO, spd::TwoParticleExcitation)
+    print(io, "TwoParticleExcitation($(spd.I), $(spd.J), $(spd.K), $(spd.L))")
 end
 
-LOStructure(::Type{T}) where T<:TwoParticleReducedDensityMatrix = AdjointUnknown()
+LOStructure(::Type{T}) where T<:TwoParticleExcitation = AdjointUnknown()
 
-function diagonal_element(spd::TwoParticleReducedDensityMatrix, add::SingleComponentFockAddress)
+function diagonal_element(spd::TwoParticleExcitation, add::SingleComponentFockAddress)
     src = find_mode(add, (spd.L, spd.K))
     dst = find_mode(add,(spd.I, spd.J))
     address, value = excitation(add, (dst...,), (src...,))
@@ -100,51 +93,17 @@ function diagonal_element(spd::TwoParticleReducedDensityMatrix, add::SingleCompo
     end
 end
 
-function num_offdiagonals(spd::TwoParticleReducedDensityMatrix, address::SingleComponentFockAddress)
+function num_offdiagonals(spd::TwoParticleExcitation, address::SingleComponentFockAddress)
     if (spd.I, spd.J) == (spd.K, spd.L)
         return 0
     else
-        return 6
+        return 1
     end
 end
 
-function get_offdiagonal(spd::TwoParticleReducedDensityMatrix, add::BoseFS, chosen)
-    if chosen<=2
-        src = find_mode(add, (spd.L, spd.K))
-        dst = find_mode(add,(spd.I, spd.J))
-    elseif chosen <=4 && chosen>2
-        src = find_mode(add, (spd.K, spd.L))
-        dst = find_mode(add,(spd.I, spd.J))
-    else
-        src = find_mode(add, (spd.L, spd.K))
-        dst = find_mode(add,(spd.J, spd.I))
-    end
-    if chosen%2 == 0
-        src, dst = dst, src
-    end
+function get_offdiagonal(spd::TwoParticleExcitation, add::SingleComponentFockAddress, chosen)
+    src = find_mode(add, (spd.L, spd.K))
+    dst = find_mode(add,(spd.I, spd.J))
     address, value = excitation(add, (dst...,), (src...,))
-    return address, value/6
+    return address, value
 end
-
-function get_offdiagonal(spd::TwoParticleReducedDensityMatrix, add::FermiFS, chosen)
-    if chosen <= 2
-        src = find_mode(add, (spd.L, spd.K))
-        dst = find_mode(add,(spd.I, spd.J))
-    elseif chosen <= 4 && chosen > 2
-        src = find_mode(add, (spd.K, spd.L))
-        dst = find_mode(add,(spd.I, spd.J))
-    else
-        src = find_mode(add, (spd.L, spd.K))
-        dst = find_mode(add,(spd.J, spd.I))
-    end
-    if chosen%2 == 0
-        src, dst = dst, src
-    end
-    address, value = excitation(add, (dst...,), (src...,))
-    if chosen <= 2
-        return address, value/6
-    else
-        return address, -value/6
-    end
-end
-
