a25073ad0be70fcb83d77552ecd0066d4d91b7ba
Skuwar1 <168614969+Skuwar1@users.noreply.github.com>
2024-09-26 22:14:27 +1200

    Update src/Hamiltonians/HubbardMom1D.jl
    Co-authored-by: Joachim Brand <joachim.brand@gmail.com>

--- src/Hamiltonians/HubbardMom1D.jl
+++ src/Hamiltonians/HubbardMom1D.jl
@@ -1,6 +1,6 @@
 """
     hubbard_dispersion(t,k)
-Dispersion relation for [`HubbardMom1D`](@ref). Returns `-2(Re(t) cos(k) + Im(t) sin(k))`.
+Dispersion relation for [`HubbardMom1D`](@ref). Returns ``-2(\\Re(t) \\cos(k) + \\Im(t) \\sin(k))``.
 
 See also [`continuum_dispersion`](@ref).
 """
