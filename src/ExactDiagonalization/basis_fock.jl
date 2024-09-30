8fd072e6995f6ba6f37838555cc9290af34f25a5
mtsch <matijacufar@gmail.com>
2024-09-30 20:58:48 +1300

    Fix doc reference

--- src/ExactDiagonalization/basis_fock.jl
+++ src/ExactDiagonalization/basis_fock.jl
@@ -6,7 +6,8 @@ Return all possible Fock states of a given type as a vector. This method is _muc
 than `build_basis(::AbstractHamiltonian, ...)`, but does not take matrix blocking into
 account. This version of `build_basis` accepts no additional arguments.
 
-All address types except [`OccupationNumberFS`](@ref) are supported.
+All address types except [`OccupationNumberFS`](@ref Main.Rimu.OccupationNumberFS) are
+supported.
 
 Returns a sorted vector of length equal to the [`dimension`](@ref) of `addr`.
 """
